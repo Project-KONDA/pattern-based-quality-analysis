b6d0c84f3d51b0185b18ce511561df6a02fe97c9
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-03-10 12:39:27 +0100

    Remove actions which are performed automatically anyway from test04_quantorcombinations.

--- qualitypatternmodel/translationtests/test04_quantorcombinations.java
+++ qualitypatternmodel/translationtests/test04_quantorcombinations.java
@@ -30,50 +30,12 @@ public class test04_quantorcombinations {
 		qcond2.setCondition(t);
 		
 		
-		// Graph Structure
+		// EXISTS 2 additional graph structure
 		GraphstructurePackage.eINSTANCE.eClass();
 		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
-
-		Graph graph = qcond2.getGraph();
-		SingleElement se = graphFactory.createSingleElement();
-		SingleElement se2 = graphFactory.createSingleElement();
-		se.getNextSingle().add(se2);
-		graph.getRootElement().getNextSingle().add(se);
-		graph.getReturnElements().add(se);
-		
-		// Morphism
-		Graph g1 = qcond.getGraph();
-		Graph g2 = qcond2.getGraph();
-		Morphism m2 = qcond2.getMorphism();
-		m2.setFrom(g1);
-		m2.setTo(g2);		
-		// SEMapping 1
-		SingleElementMapping sem = (SingleElementMapping) m2.getMappings().get(0);
-		sem.setFrom(g1.getRootElement());
-		sem.setTo(g2.getRootElement());
-		// SEMapping 2
-		SingleElementMapping sem2 = factory.createSingleElementMapping();
-		m2.getMappings().add(sem2);
-		sem2.setFrom(g1.getRootElement().getNextSingle().get(0));
-		sem2.setTo(g2.getRootElement().getNextSingle().get(0));
-		// RMapping 1
-		RelationMapping rm1 = factory.createRelationMapping();
-		m2.getMappings().add(rm1);
-		rm1.setFrom(g1.getRootElement().getNextSingle().get(0).getRelationFromPrevious());		
-		rm1.setTo(g2.getRootElement().getNextSingle().get(0).getRelationFromPrevious());
-		// SEMapping 3
-		SingleElementMapping sem3 = factory.createSingleElementMapping();
-		m2.getMappings().add(sem3);
-		sem3.setFrom(g1.getRootElement().getNextSingle().get(0).getNextSingle().get(0));
-		sem3.setTo(g2.getRootElement().getNextSingle().get(0).getNextSingle().get(0));
-		// RMapping 2
-		RelationMapping rm2 = factory.createRelationMapping();
-		m2.getMappings().add(rm2);
-		rm2.setFrom(g1.getRootElement().getNextSingle().get(0).getNextSingle().get(0).getRelationFromPrevious());
-		rm2.setTo(g2.getRootElement().getNextSingle().get(0).getNextSingle().get(0).getRelationFromPrevious());
 		
 		SingleElement se3 = graphFactory.createSingleElement();
-		se2.getNextSingle().add(se3);
+		qcond2.getGraph().getRootElement().getNextSingle().get(0).getNextSingle().get(0).getNextSingle().add(se3);
 		
 		return pattern;
 	}
