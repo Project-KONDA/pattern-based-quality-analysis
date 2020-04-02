bd8f9c1641abf2a39221a01770dfd97712376125
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-04-02 12:13:35 +0200

    Implement Contrel instance for MIDAS

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval10Contrel.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval10Contrel.java
@@ -2,16 +2,22 @@ package qualitypatternmodel.evaluation;
 
 import java.util.ArrayList;
 
+import qualitypatternmodel.graphstructure.Axis;
 import qualitypatternmodel.graphstructure.Element;
 import qualitypatternmodel.graphstructure.Graph;
 import qualitypatternmodel.graphstructure.GraphstructureFactory;
 import qualitypatternmodel.graphstructure.GraphstructurePackage;
 import qualitypatternmodel.graphstructure.Property;
+import qualitypatternmodel.graphstructure.PropertyLocation;
 import qualitypatternmodel.graphstructure.ReturnType;
 import qualitypatternmodel.operators.Comparison;
 import qualitypatternmodel.operators.ComparisonOperator;
 import qualitypatternmodel.operators.FunctionsFactory;
 import qualitypatternmodel.operators.FunctionsPackage;
+import qualitypatternmodel.parameters.ParametersFactory;
+import qualitypatternmodel.parameters.ParametersPackage;
+import qualitypatternmodel.parameters.TextLiteralParam;
+import qualitypatternmodel.parameters.UnknownParameterValue;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.PatternstructureFactory;
 import qualitypatternmodel.patternstructure.PatternstructurePackage;
@@ -22,7 +28,7 @@ import qualitypatternmodel.translationtests.Test00;
 public class Eval10Contrel {
 	public static void main(String[] args) {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
-		completePatterns.add(getContrelAbstract());
+		completePatterns.add(getContrelMidas());
 		Test00.test(completePatterns);		
 	}
 	
@@ -65,6 +71,7 @@ public class Eval10Contrel {
 		
 		Element element2 = graphFactory.createElement();
 		element2.setPreviousElement(graph.getRootElement());
+		element2.addPrimitiveComparison();
 		Element id2 = graphFactory.createElement();
 		id2.setPreviousElement(element2);
 		id2.addPrimitiveComparison();
@@ -105,5 +112,105 @@ public class Eval10Contrel {
 		compRefValue.getOption().setValue(ComparisonOperator.NOTEQUAL);		
 		
 		return pattern;
+	}
+	
+	public static CompletePattern getContrelMidas() {
+		ParametersPackage.eINSTANCE.eClass();
+		ParametersFactory parametersFactory = ParametersFactory.eINSTANCE;
+		
+		CompletePattern completePattern = getContrelAbstract();
+		Element returnElementInReturnGraph = completePattern.getGraph().getReturnElements().get(0);	
+		returnElementInReturnGraph.getRelationFromPrevious().getOption().setValue(Axis.DESCENDANT);
+		
+		Comparison comparisonReturnElementInReturnGraph = (Comparison) returnElementInReturnGraph.getPredicates().get(0);
+		TextLiteralParam concreteInputValue = parametersFactory.createTextLiteralParam();
+		concreteInputValue.setValue("kue");
+		((UnknownParameterValue) comparisonReturnElementInReturnGraph.getArguments().get(1)).concretize(concreteInputValue);
+		returnElementInReturnGraph.getProperties().get(0).getAttributeName().setValue("Type");
+		returnElementInReturnGraph.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
+		Element root = graph1.getRootElement();
+		Element returnElementInGraph1 = graph1.getReturnElements().get(0);
+		
+		Element id1 = returnElementInGraph1.getNextElements().get(0);
+		Comparison id1Comp1 = (Comparison) id1.getPredicates().get(0);
+		TextLiteralParam concreteInputValue1 = parametersFactory.createTextLiteralParam();
+		concreteInputValue1.setValue("3100");
+		((UnknownParameterValue) id1Comp1.getArguments().get(1)).concretize(concreteInputValue1);
+		id1.getProperties().get(0).getAttributeName().setValue("Type");
+		id1.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		id1.getProperties().get(1).getAttributeName().setValue("Value");
+		id1.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Element ref1 = returnElementInGraph1.getNextElements().get(1);
+		Comparison ref1Comp1 = (Comparison) ref1.getPredicates().get(0);
+		TextLiteralParam concreteInputValue2 = parametersFactory.createTextLiteralParam();
+		concreteInputValue2.setValue("ku35");
+		((UnknownParameterValue) ref1Comp1.getArguments().get(1)).concretize(concreteInputValue2);
+		ref1.getProperties().get(0).getAttributeName().setValue("Type");
+		ref1.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		ref1.getProperties().get(1).getAttributeName().setValue("Value");
+		ref1.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Element val1 = ref1.getNextElements().get(0);	
+		Comparison val1Comp1 = (Comparison) val1.getPredicates().get(0);
+		TextLiteralParam concreteInputValue3 = parametersFactory.createTextLiteralParam();
+		concreteInputValue3.setValue("3600");
+		((UnknownParameterValue) val1Comp1.getArguments().get(1)).concretize(concreteInputValue3);
+		val1.getProperties().get(0).getAttributeName().setValue("Type");
+		val1.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		val1.getProperties().get(1).getAttributeName().setValue("Value");
+		val1.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Element element2 = root.getNextElements().get(1);
+		element2.getRelationFromPrevious().getOption().setValue(Axis.DESCENDANT);
+		Comparison element2Comp1 = (Comparison) element2.getPredicates().get(0);
+		TextLiteralParam concreteInputValue4 = parametersFactory.createTextLiteralParam();
+		concreteInputValue4.setValue("wer");
+		((UnknownParameterValue) element2Comp1.getArguments().get(1)).concretize(concreteInputValue4);
+		element2.getProperties().get(0).getAttributeName().setValue("Type");
+		element2.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Element id2 = element2.getNextElements().get(0);	
+		Comparison id2Comp1 = (Comparison) id2.getPredicates().get(0);
+		TextLiteralParam concreteInputValue5 = parametersFactory.createTextLiteralParam();
+		concreteInputValue5.setValue("3600");
+		((UnknownParameterValue) id2Comp1.getArguments().get(1)).concretize(concreteInputValue5);
+		id2.getProperties().get(0).getAttributeName().setValue("Type");
+		id2.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		id2.getProperties().get(1).getAttributeName().setValue("Value");
+		id2.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);		
+		
+		Element ref2 = element2.getNextElements().get(1);
+		Comparison ref2Comp1 = (Comparison) ref2.getPredicates().get(0);
+		TextLiteralParam concreteInputValue6 = parametersFactory.createTextLiteralParam();
+		concreteInputValue6.setValue("we30");
+		((UnknownParameterValue) ref2Comp1.getArguments().get(1)).concretize(concreteInputValue6);
+		ref2.getProperties().get(0).getAttributeName().setValue("Type");
+		ref2.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		ref2.getProperties().get(1).getAttributeName().setValue("Value");
+		ref2.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);		
+		
+		Element val2 = ref2.getNextElements().get(0);	
+		Comparison val2Comp1 = (Comparison) val2.getPredicates().get(0);
+		TextLiteralParam concreteInputValue7 = parametersFactory.createTextLiteralParam();
+		concreteInputValue7.setValue("3100");
+		((UnknownParameterValue) val2Comp1.getArguments().get(1)).concretize(concreteInputValue7);
+		val2.getProperties().get(0).getAttributeName().setValue("Type");
+		val2.getProperties().get(0).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		val2.getProperties().get(1).getAttributeName().setValue("Value");
+		val2.getProperties().get(1).getOption().setValue(PropertyLocation.ATTRIBUTE);
+		
+		Comparison comparison1 = (Comparison) id1.getPredicates().get(1);
+		comparison1.setType(ReturnType.STRING);
+		
+		Comparison comparison2 = (Comparison) val1.getPredicates().get(1);
+		comparison2.setType(ReturnType.STRING);
+		
+		Comparison comparison3 = (Comparison) ref1.getPredicates().get(1);
+		comparison3.setType(ReturnType.STRING);
+				
+		return completePattern;
 	}
 }
