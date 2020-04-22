f69fdb755d443f94f56960cae914188f85c5d8d9
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-04-22 13:45:16 +0200

    Rework Eval03Refint

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval03Refint.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval03Refint.java
@@ -6,171 +6,200 @@ import qualitypatternmodel.graphstructure.Graph;
 import qualitypatternmodel.graphstructure.GraphstructureFactory;
 import qualitypatternmodel.graphstructure.GraphstructurePackage;
 import qualitypatternmodel.graphstructure.Property;
-import qualitypatternmodel.graphstructure.PropertyLocation;
+import qualitypatternmodel.graphstructure.Relation;
 import qualitypatternmodel.graphstructure.ReturnType;
 import qualitypatternmodel.operators.Comparison;
-import qualitypatternmodel.operators.FunctionsFactory;
-import qualitypatternmodel.operators.FunctionsPackage;
+import qualitypatternmodel.operators.OperatorsFactory;
+import qualitypatternmodel.operators.OperatorsPackage;
 import qualitypatternmodel.parameters.ParametersFactory;
 import qualitypatternmodel.parameters.ParametersPackage;
 import qualitypatternmodel.parameters.TextLiteralParam;
 import qualitypatternmodel.parameters.UnknownParameterValue;
-import qualitypatternmodel.graphstructure.Axis;
+import qualitypatternmodel.adaptionxml.PropertyKind;
+import qualitypatternmodel.adaptionxml.RelationKind;
+import qualitypatternmodel.adaptionxml.XmlElement;
+import qualitypatternmodel.adaptionxml.XmlNavigation;
+import qualitypatternmodel.adaptionxml.XmlProperty;
+import qualitypatternmodel.adaptionxml.XmlReference;
 import qualitypatternmodel.graphstructure.Element;
 import qualitypatternmodel.patternstructure.NotCondition;
+import qualitypatternmodel.patternstructure.PatternstructureFactory;
+import qualitypatternmodel.patternstructure.PatternstructurePackage;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.QuantifiedCondition;
+import qualitypatternmodel.patternstructure.TrueElement;
 import qualitypatternmodel.translationtests.Test00;
+import qualitypatternmodel.translationtests.Test03Quantor;
 import qualitypatternmodel.translationtests.Test06NotElement;
 
 public class Eval03Refint {
 	public static void main(String[] args) {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
-		completePatterns.add(getRefintMidas());
+		completePatterns.add(getRefintRunningExample());
 		Test00.test(completePatterns);
 		
 	}
-	public static CompletePattern getRefintAbstract() {
-		GraphstructurePackage.eINSTANCE.eClass();
-		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
-		
-		FunctionsPackage.eINSTANCE.eClass();
-		FunctionsFactory functionsFactory = FunctionsFactory.eINSTANCE;		
-		
-		CompletePattern completePattern = Test06NotElement.getPatternExistsNotExists();
-		Element returnElementInReturnGraph = completePattern.getGraph().getReturnElements().get(0);		
-		
-		returnElementInReturnGraph.addPrimitiveComparison();		
-		
-		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
-		Element returnElementInGraph1 = graph1.getReturnElements().get(0);				
-		Element nextToReturnElementInGraph1 = returnElementInGraph1.getNextElements().get(0);					
-		nextToReturnElementInGraph1.addPrimitiveComparison(); 	
-		
-		Graph graph2 = ((QuantifiedCondition) ((NotCondition) ((QuantifiedCondition) completePattern.getCondition()).getCondition()).getCondition()).getGraph();
-		Element rootElementInGraph2 = graph2.getRootElement();
+	
+	public static CompletePattern getRefintAbstractMidas() {
+		PatternstructurePackage.eINSTANCE.eClass();
+		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
 
-		Element returnElementInGraph2 = graph2.getReturnElements().get(0);
-		Element nextToReturnElementInGraph2 = returnElementInGraph2.getNextElements().get(0);	
-		Property propertyNextToReturnElementInGraph2 = graphFactory.createProperty();
-		propertyNextToReturnElementInGraph2.setElement(nextToReturnElementInGraph2);
-		propertyNextToReturnElementInGraph2.createParameters();
+		GraphstructurePackage.eINSTANCE.eClass();
+		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;		
+				
+		CompletePattern completePattern = Test03Quantor.getPatternExists();
+		TrueElement t = (TrueElement) ((QuantifiedCondition) completePattern.getCondition()).getCondition();
+		QuantifiedCondition qc1 = (QuantifiedCondition) completePattern.getCondition(); 
+		NotCondition n = factory.createNotCondition();
+		QuantifiedCondition qc2 = factory.createQuantifiedCondition();
+				
+		qc1.setCondition(n);
+		n.setCondition(qc2);
+		qc2.setCondition(t);
 		
-		nextToReturnElementInGraph2.getNextElements().clear();
+		Element e1g0 = completePattern.getGraph().getElements().get(0);
+		e1g0.addPrimitiveComparison();
 		
-		Element otherRecordInGraph2 = graphFactory.createElement();
-		rootElementInGraph2.getNextElements().add(otherRecordInGraph2);
-		otherRecordInGraph2.addPrimitiveComparison();
+		Element e1g1 = qc1.getGraph().getElements().get(0);	
 		
-		Element nextToOtherRecordInGraph2 = graphFactory.createElement();
-		otherRecordInGraph2.getNextElements().add(nextToOtherRecordInGraph2);
-		nextToOtherRecordInGraph2.addPrimitiveComparison();
-		Property propertyNextToOtherRecordInGraph2 = graphFactory.createProperty();
-		propertyNextToOtherRecordInGraph2.setElement(nextToOtherRecordInGraph2);
-		propertyNextToOtherRecordInGraph2.createParameters();
+		Element e2g1 = qc1.getGraph().getElements().get(1);	
+		e2g1.addPrimitiveComparison();
 		
-		Comparison comparison = functionsFactory.createComparison();
-		graph2.getOperatorList().add(comparison);
-		comparison.createParameters();
-		comparison.setArgument1(propertyNextToReturnElementInGraph2);
-		comparison.setArgument2(propertyNextToOtherRecordInGraph2);
+		Relation relation = graphFactory.createRelation();
+		relation.setGraph(qc1.getGraph());
+		relation.setSource(e1g1);
+		relation.setTarget(e2g1);
 		
-		return completePattern;
+		Element e2g2 = qc2.getGraph().getElements().get(1);
+		
+		Element e3g2 = graphFactory.createElement();
+		e3g2.setGraph(qc2.getGraph());
+		e3g2.addPrimitiveComparison();
+		
+		Element e4g2 = graphFactory.createElement();
+		e4g2.setGraph(qc2.getGraph());
+		e4g2.addPrimitiveComparison();
+		
+		Relation relationg2 = graphFactory.createRelation();
+		relationg2.setGraph(qc2.getGraph());
+		relationg2.setSource(e3g2);
+		relationg2.setTarget(e4g2);
+		
+		Relation referenceg2 = graphFactory.createRelation();
+		referenceg2.setGraph(qc2.getGraph());
+		referenceg2.setSource(e2g2);
+		referenceg2.setTarget(e4g2);
+		
+		completePattern.createXMLAdaption();
+		relation.adaptAsXMLNavigation();
+		relationg2.adaptAsXMLNavigation();
+		XmlReference xmlReference = referenceg2.adaptAsXMLReference();
+		xmlReference.setType(ReturnType.STRING);
+		completePattern.finalizeXMLAdaption();	
+		
+		return completePattern; 
 	}
 	
-	public static CompletePattern getRefintMidas() {
+	public static CompletePattern getRefintMidasWer() {
 		ParametersPackage.eINSTANCE.eClass();
 		ParametersFactory parametersFactory = ParametersFactory.eINSTANCE;
 		
-		CompletePattern completePattern = getRefintAbstract();
+		CompletePattern completePattern = getRefintAbstractMidas();
 		
-		Element returnInReturnGraph = completePattern.getGraph().getRootElement().getNextElements().get(0);
-		returnInReturnGraph.getRelationFromPrevious().getOption().setValue(RelationKind.DESCENDANT);
+		Element returnInReturnGraph = completePattern.getGraph().getElements().get(0);
+		XmlNavigation relation1 = (XmlNavigation) completePattern.getGraph().getRelations().get(0);
+		relation1.getOption().setValue(RelationKind.DESCENDANT);
 		TextLiteralParam concreteInputValue = parametersFactory.createTextLiteralParam();
 		concreteInputValue.setValue("obj");
 		((UnknownParameterValue) ((Comparison) returnInReturnGraph.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue);
-		returnInReturnGraph.getProperties().get(0).getAttributeName().setValue("Type");
-		returnInReturnGraph.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) returnInReturnGraph.getProperties().get(0)).getAttributeName().setValue("Type");
+		((XmlProperty) returnInReturnGraph.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);
 		
 		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
-		Element element1 = graph1.getRootElement().getNextElements().get(0).getNextElements().get(0);
-		element1.getRelationFromPrevious().getOption().setValue(RelationKind.DESCENDANT);
+		Element element1 = graph1.getElements().get(1);
+		XmlNavigation relation2 = (XmlNavigation) graph1.getRelations().get(0);
+		relation2.getOption().setValue(RelationKind.DESCENDANT);
 		TextLiteralParam concreteInputValue2 = parametersFactory.createTextLiteralParam();
 		concreteInputValue2.setValue("3600");
 		((UnknownParameterValue) ((Comparison) element1.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue2);
-		element1.getProperties().get(0).getAttributeName().setValue("Type");
-		element1.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element1.getProperties().get(0)).getAttributeName().setValue("Type");
+		((XmlProperty) element1.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);
 		
 		Graph graph2 = ((QuantifiedCondition) ((NotCondition) ((QuantifiedCondition) completePattern.getCondition()).getCondition()).getCondition()).getGraph();
-		Element element1InGraph2 = graph2.getRootElement().getNextElements().get(0).getNextElements().get(0);
-		Element element2 = graph2.getRootElement().getNextElements().get(1);
-		element2.getRelationFromPrevious().getOption().setValue(RelationKind.DESCENDANT);
+		Element element1InGraph2 = graph2.getElements().get(1);
+		Element element2 = graph2.getElements().get(2);
+		XmlNavigation relation3 = (XmlNavigation) graph2.getRelations().get(3);
+		relation3.getOption().setValue(RelationKind.DESCENDANT);
 		TextLiteralParam concreteInputValue3 = parametersFactory.createTextLiteralParam();
 		concreteInputValue3.setValue("wer");
 		((UnknownParameterValue) ((Comparison) element2.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue3);
-		element2.getProperties().get(0).getAttributeName().setValue("Type");
-		element2.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element2.getProperties().get(0)).getAttributeName().setValue("Type");
+		((XmlProperty) element2.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);
 		
-		Element element3 = element2.getNextElements().get(0);
+		Element element3 = graph2.getElements().get(3);
 		TextLiteralParam concreteInputValue4 = parametersFactory.createTextLiteralParam();
 		concreteInputValue4.setValue("3600");
 		((UnknownParameterValue) ((Comparison) element3.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue4);
-		element3.getProperties().get(0).getAttributeName().setValue("Type");
-		element3.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);		
+		((XmlProperty) element3.getProperties().get(0)).getAttributeName().setValue("Type");
+		((XmlProperty) element3.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);		
 		
-		element1InGraph2.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);
-		element1InGraph2.getProperties().get(0).getAttributeName().setValue("Value");
+		((XmlProperty) element1InGraph2.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element1InGraph2.getProperties().get(0)).getAttributeName().setValue("Value");
 		
-		element3.getProperties().get(1).getOption().setValue(PropertyKind.ATTRIBUTE);
-		element3.getProperties().get(1).getAttributeName().setValue("Value");
-		
-		((Comparison) element3.getPredicates().get(1)).setType(ReturnType.STRING);
-		
+		((XmlProperty) element3.getProperties().get(1)).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element3.getProperties().get(1)).getAttributeName().setValue("Value");
+				
 		return completePattern;		
 	}
 	
 	public static CompletePattern getRefintAbstractRunningExample() {
-		GraphstructurePackage.eINSTANCE.eClass();
-		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
-		
-		FunctionsPackage.eINSTANCE.eClass();
-		FunctionsFactory functionsFactory = FunctionsFactory.eINSTANCE;		
-		
-		CompletePattern completePattern = Test06NotElement.getPatternExistsNotExists();
-		Element returnElementInReturnGraph = completePattern.getGraph().getReturnElements().get(0);		
-		
-		returnElementInReturnGraph.addPrimitiveComparison();		
-		
-		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
-		Element returnElementInGraph1 = graph1.getReturnElements().get(0);				
-		Element nextToReturnElementInGraph1 = returnElementInGraph1.getNextElements().get(0);					
-		nextToReturnElementInGraph1.addPrimitiveComparison(); 	
-		
-		Graph graph2 = ((QuantifiedCondition) ((NotCondition) ((QuantifiedCondition) completePattern.getCondition()).getCondition()).getCondition()).getGraph();
-		Element rootElementInGraph2 = graph2.getRootElement();
+		PatternstructurePackage.eINSTANCE.eClass();
+		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
 
-		Element returnElementInGraph2 = graph2.getReturnElements().get(0);
-		Element nextToReturnElementInGraph2 = returnElementInGraph2.getNextElements().get(0);
-		nextToReturnElementInGraph2.getNextElements().clear();
-		Property propertyNextToReturnElementInGraph2 = graphFactory.createProperty();
-		propertyNextToReturnElementInGraph2.setElement(nextToReturnElementInGraph2);
-		propertyNextToReturnElementInGraph2.createParameters();
+		GraphstructurePackage.eINSTANCE.eClass();
+		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;		
+				
+		CompletePattern completePattern = Test03Quantor.getPatternExists();
+		TrueElement t = (TrueElement) ((QuantifiedCondition) completePattern.getCondition()).getCondition();
+		QuantifiedCondition qc1 = (QuantifiedCondition) completePattern.getCondition(); 
+		NotCondition n = factory.createNotCondition();
+		QuantifiedCondition qc2 = factory.createQuantifiedCondition();
+				
+		qc1.setCondition(n);
+		n.setCondition(qc2);
+		qc2.setCondition(t);
 		
-		Element otherRecordInGraph2 = graphFactory.createElement();
-		rootElementInGraph2.getNextElements().add(otherRecordInGraph2);
-		otherRecordInGraph2.addPrimitiveComparison();		
+		Element e1g0 = completePattern.getGraph().getElements().get(0);
+		e1g0.addPrimitiveComparison();
 		
-		Property propertyNextToOtherRecordInGraph2 = graphFactory.createProperty();
-		propertyNextToOtherRecordInGraph2.setElement(otherRecordInGraph2);
-		propertyNextToOtherRecordInGraph2.createParameters();
+		Element e1g1 = qc1.getGraph().getElements().get(0);	
 		
-		Comparison comparison = functionsFactory.createComparison();
-		graph2.getOperatorList().add(comparison);
-		comparison.createParameters();
-		comparison.setArgument1(propertyNextToReturnElementInGraph2);
-		comparison.setArgument2(propertyNextToOtherRecordInGraph2);
+		Element e2g1 = qc1.getGraph().getElements().get(1);	
+		e2g1.addPrimitiveComparison();
 		
-		return completePattern;
+		Relation relation = graphFactory.createRelation();
+		relation.setGraph(qc1.getGraph());
+		relation.setSource(e1g1);
+		relation.setTarget(e2g1);
+		
+		Element e2g2 = qc2.getGraph().getElements().get(1);
+		
+		Element e3g2 = graphFactory.createElement();
+		e3g2.setGraph(qc2.getGraph());
+		e3g2.addPrimitiveComparison();		
+		
+		Relation referenceg2 = graphFactory.createRelation();
+		referenceg2.setGraph(qc2.getGraph());
+		referenceg2.setSource(e2g2);
+		referenceg2.setTarget(e3g2);
+		
+		completePattern.createXMLAdaption();
+		relation.adaptAsXMLNavigation();
+		XmlReference xmlReference = referenceg2.adaptAsXMLReference();
+		xmlReference.setType(ReturnType.STRING);
+		completePattern.finalizeXMLAdaption();	
+		
+		return completePattern; 
 	}
 	
 	public static CompletePattern getRefintRunningExample() {
@@ -179,35 +208,33 @@ public class Eval03Refint {
 		
 		CompletePattern completePattern = getRefintAbstractRunningExample();
 		
-		Element returnInReturnGraph = completePattern.getGraph().getRootElement().getNextElements().get(0);
+		XmlElement returnInReturnGraph = (XmlElement) completePattern.getGraph().getElements().get(0);
 		TextLiteralParam concreteInputValue = parametersFactory.createTextLiteralParam();
 		concreteInputValue.setValue("building");
 		((UnknownParameterValue) ((Comparison) returnInReturnGraph.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue);
-		returnInReturnGraph.getProperties().get(0).getOption().setValue(PropertyKind.TAG);
+		((XmlProperty) returnInReturnGraph.getProperties().get(0)).getOption().setValue(PropertyKind.TAG);
 		
 		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
-		Element element1 = graph1.getRootElement().getNextElements().get(0).getNextElements().get(0);
+		Element element1 = graph1.getElements().get(1);
 		TextLiteralParam concreteInputValue2 = parametersFactory.createTextLiteralParam();
 		concreteInputValue2.setValue("creator");
 		((UnknownParameterValue) ((Comparison) element1.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue2);
-		element1.getProperties().get(0).getOption().setValue(PropertyKind.TAG);
+		((XmlProperty) element1.getProperties().get(0)).getOption().setValue(PropertyKind.TAG);
 		
 		Graph graph2 = ((QuantifiedCondition) ((NotCondition) ((QuantifiedCondition) completePattern.getCondition()).getCondition()).getCondition()).getGraph();
-		Element element1InGraph2 = graph2.getRootElement().getNextElements().get(0).getNextElements().get(0);
-		Element element2 = graph2.getRootElement().getNextElements().get(1);
+		Element element1InGraph2 = graph2.getElements().get(1);
+		Element element2 = graph2.getElements().get(2);
 		TextLiteralParam concreteInputValue3 = parametersFactory.createTextLiteralParam();
 		concreteInputValue3.setValue("artist");
 		((UnknownParameterValue) ((Comparison) element2.getPredicates().get(0)).getArgument2()).concretize(concreteInputValue3);
-		element2.getProperties().get(0).getOption().setValue(PropertyKind.TAG);
+		((XmlProperty) element2.getProperties().get(0)).getOption().setValue(PropertyKind.TAG);
 		
-		element2.getProperties().get(1).getOption().setValue(PropertyKind.ATTRIBUTE);
-		element2.getProperties().get(1).getAttributeName().setValue("id");
+		((XmlProperty) element2.getProperties().get(1)).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element2.getProperties().get(1)).getAttributeName().setValue("id");
 		
-		element1InGraph2.getProperties().get(0).getOption().setValue(PropertyKind.ATTRIBUTE);
-		element1InGraph2.getProperties().get(0).getAttributeName().setValue("ref");
-		
-		((Comparison) element2.getPredicates().get(1)).setType(ReturnType.STRING);
-		
+		((XmlProperty) element1InGraph2.getProperties().get(0)).getOption().setValue(PropertyKind.ATTRIBUTE);
+		((XmlProperty) element1InGraph2.getProperties().get(0)).getAttributeName().setValue("ref");
+				
 		return completePattern;		
 	}
 }
