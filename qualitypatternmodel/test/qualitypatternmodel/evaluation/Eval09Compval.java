453a8aab7fa3f11dbf4de0bbad89e19486c41ada
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-04-23 10:28:05 +0200

    Add Lido compval instances. Refactoring.

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval09Compval.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/Eval09Compval.java
@@ -10,6 +10,9 @@ import qualitypatternmodel.adaptionxml.XmlNavigation;
 import qualitypatternmodel.adaptionxml.XmlProperty;
 import qualitypatternmodel.graphstructure.Element;
 import qualitypatternmodel.graphstructure.Graph;
+import qualitypatternmodel.graphstructure.GraphstructureFactory;
+import qualitypatternmodel.graphstructure.GraphstructurePackage;
+import qualitypatternmodel.graphstructure.Relation;
 import qualitypatternmodel.operators.Comparison;
 import qualitypatternmodel.parameters.ParametersFactory;
 import qualitypatternmodel.parameters.ParametersPackage;
@@ -24,138 +27,258 @@ import qualitypatternmodel.translationtests.Test03Quantor;
 public class Eval09Compval {
 	public static void main(String[] args) {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
-		completePatterns.add(getCompvalLidoEventPlace());
+		completePatterns.add(getCompvalLidoObjectWorkTypePrint());
 		Test00.test(completePatterns);
-		
+
 	}
-	public static CompletePattern getCompvalAbstract() {	
-		
+
+	public static CompletePattern getCompvalAbstract() {
+
 		CompletePattern completePattern = Test03Quantor.getPatternExistsWithRelation();
-		Element returnElementInReturnGraph = completePattern.getGraph().getElements().get(0);			
-		returnElementInReturnGraph.addPrimitiveComparison(); 
-		
+		Element returnElementInReturnGraph = completePattern.getGraph().getElements().get(0);
+		returnElementInReturnGraph.addPrimitiveComparison();
+
 		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
-		
-		Element next1 = graph1.getElements().get(1);	
+
+		Element next1 = graph1.getElements().get(1);
 		next1.addPrimitiveComparison();
 		next1.addPrimitiveComparison();
-		
+
 		completePattern.createXMLAdaption();
 		graph1.getRelations().get(0).adaptAsXMLNavigation();
 		completePattern.finalizeXMLAdaption();
-		
+
 		return completePattern;
 	}
-	
-	public static CompletePattern getCompvalConcrete(String returnElementType, RelationKind returnElementAxis, String attribute1Name, PropertyKind attribute1Kind,
-			String element2Type, RelationKind element2Axis, String attribute2Name, PropertyKind attribute2Kind, String attribute3Name, PropertyKind attribute3Kind, String value) {
+
+	public static CompletePattern getCompvalThreeElementsAbstract() {
+
+		GraphstructurePackage.eINSTANCE.eClass();
+		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
+
+		CompletePattern completePattern = Test03Quantor.getPatternExistsWithRelation();
+		Element returnElementInReturnGraph = completePattern.getGraph().getElements().get(0);
+		returnElementInReturnGraph.addPrimitiveComparison();
+
+		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
+
+		Element element1 = graph1.getElements().get(1);
+		element1.addPrimitiveComparison();
+		
+
+		Element element2 = graphFactory.createElement();
+		element2.setGraph(graph1);
+		element2.addPrimitiveComparison();
+		element2.addPrimitiveComparison();
+
+		Relation relation = graphFactory.createRelation();
+		relation.setGraph(graph1);
+		relation.setSource(element1);
+		relation.setTarget(element2);
+
+		completePattern.createXMLAdaption();
+		graph1.getRelations().get(0).adaptAsXMLNavigation();
+		relation.adaptAsXMLNavigation();
+		completePattern.finalizeXMLAdaption();
+
+		return completePattern;
+	}
+
+	public static CompletePattern getCompvalThreeElementsConcrete(String returnElementType,
+			RelationKind returnElementAxis, String attribute1Name, PropertyKind attribute1Kind, String element2Type,
+			RelationKind element2Axis, String attribute2Name, PropertyKind attribute2Kind, String element3Type,
+			RelationKind element3Axis, String attribute3Name, PropertyKind attribute3Kind, String attribute4Name,
+			PropertyKind attribute4Kind, String value) {
 		ParametersPackage.eINSTANCE.eClass();
 		ParametersFactory parametersFactory = ParametersFactory.eINSTANCE;
-		
-		CompletePattern completePattern = getCompvalAbstract();
-		
-		Element returnElementInReturnGraph = completePattern.getGraph().getReturnElements().get(0);	
+
+		CompletePattern completePattern = getCompvalThreeElementsAbstract();
+
+		Element returnElementInReturnGraph = completePattern.getGraph().getElements().get(0);
 		((XmlNavigation) completePattern.getGraph().getRelations().get(0)).getOption().setValue(returnElementAxis);
-		
-		Comparison comparisonReturnElementInReturnGraph = (Comparison) returnElementInReturnGraph.getPredicates().get(0);
+
+		Comparison comparisonReturnElementInReturnGraph = (Comparison) returnElementInReturnGraph.getPredicates()
+				.get(0);
 		TextLiteralParam concreteInputValue = parametersFactory.createTextLiteralParam();
 		concreteInputValue.setValue(returnElementType);
-		((UnknownParameterValue) comparisonReturnElementInReturnGraph.getArguments().get(1)).concretize(concreteInputValue);
-		((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getAttributeName().setValue(attribute1Name);
+		((UnknownParameterValue) comparisonReturnElementInReturnGraph.getArguments().get(1))
+				.concretize(concreteInputValue);
+		if (attribute1Kind == PropertyKind.ATTRIBUTE) {
+			((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getAttributeName()
+					.setValue(attribute1Name);
+		}
 		((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getOption().setValue(attribute1Kind);
-		
-		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();		
-		
+
+		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
+
 		Element element2 = graph1.getElements().get(1);
 		((XmlNavigation) graph1.getRelations().get(0)).getOption().setValue(element2Axis);
-		
+
 		Comparison comp1 = (Comparison) element2.getPredicates().get(0);
 		TextLiteralParam concreteInputValue2 = parametersFactory.createTextLiteralParam();
 		concreteInputValue2.setValue(element2Type);
 		((UnknownParameterValue) comp1.getArguments().get(1)).concretize(concreteInputValue2);
-		if(attribute2Kind == PropertyKind.ATTRIBUTE) {
+		if (attribute2Kind == PropertyKind.ATTRIBUTE) {
 			((XmlProperty) element2.getProperties().get(0)).getAttributeName().setValue(attribute2Name);
 		}
 		((XmlProperty) element2.getProperties().get(0)).getOption().setValue(attribute2Kind);
-		
+
+		Element element3 = graph1.getElements().get(2);
+		((XmlNavigation) graph1.getRelations().get(1)).getOption().setValue(element3Axis);
+
+		Comparison comp2 = (Comparison) element3.getPredicates().get(0);
+		TextLiteralParam concreteInputValue3 = parametersFactory.createTextLiteralParam();
+		concreteInputValue3.setValue(element3Type);
+		((UnknownParameterValue) comp2.getArguments().get(1)).concretize(concreteInputValue3);
+		if (attribute3Kind == PropertyKind.ATTRIBUTE) {
+			((XmlProperty) element3.getProperties().get(0)).getAttributeName().setValue(attribute3Name);
+		}
+		((XmlProperty) element3.getProperties().get(0)).getOption().setValue(attribute3Kind);
+
+		Comparison comp3 = (Comparison) element3.getPredicates().get(1);
+		TextLiteralParam concreteInputValue4 = parametersFactory.createTextLiteralParam();
+		concreteInputValue4.setValue(value);
+		((UnknownParameterValue) comp3.getArguments().get(1)).concretize(concreteInputValue4);
+		if (attribute3Kind == PropertyKind.ATTRIBUTE) {
+			((XmlProperty) element3.getProperties().get(1)).getAttributeName().setValue(attribute4Name);
+		}
+		((XmlProperty) element3.getProperties().get(1)).getOption().setValue(attribute4Kind);
+
+		return completePattern;
+	}
+
+	public static CompletePattern getCompvalConcrete(String returnElementType, RelationKind returnElementAxis,
+			String attribute1Name, PropertyKind attribute1Kind, String element2Type, RelationKind element2Axis,
+			String attribute2Name, PropertyKind attribute2Kind, String attribute3Name, PropertyKind attribute3Kind,
+			String value) {
+		ParametersPackage.eINSTANCE.eClass();
+		ParametersFactory parametersFactory = ParametersFactory.eINSTANCE;
+
+		CompletePattern completePattern = getCompvalAbstract();
+
+		Element element2 = concretize(returnElementType, returnElementAxis, attribute1Name, attribute1Kind,
+				element2Type, element2Axis, attribute2Name, attribute2Kind, attribute3Name, attribute3Kind, parametersFactory,
+				completePattern);
+
 		Comparison comp2 = (Comparison) element2.getPredicates().get(1);
+
 		TextLiteralParam concreteInputValue3 = parametersFactory.createTextLiteralParam();
 		concreteInputValue3.setValue(value);
 		((UnknownParameterValue) comp2.getArguments().get(1)).concretize(concreteInputValue3);
-		if(attribute3Kind == PropertyKind.ATTRIBUTE) {
-			((XmlProperty) element2.getProperties().get(1)).getAttributeName().setValue(attribute3Name);
-		}
-		((XmlProperty) element2.getProperties().get(1)).getOption().setValue(attribute3Kind);
-		
-		return completePattern;		
+
+		return completePattern;
 	}
-	
-	public static CompletePattern getCompvalSetConcrete(String returnElementType, RelationKind returnElementAxis, String attribute1Name, PropertyKind attribute1Kind,
-			String element2Type, RelationKind element2Axis, String attribute2Name, PropertyKind attribute2Kind, String attribute3Name, PropertyKind attribute3Kind, List<String> values) {
+
+	public static CompletePattern getCompvalSetConcrete(String returnElementType, RelationKind returnElementAxis,
+			String attribute1Name, PropertyKind attribute1Kind, String element2Type, RelationKind element2Axis,
+			String attribute2Name, PropertyKind attribute2Kind, String attribute3Name, PropertyKind attribute3Kind,
+			List<String> values) {
+
 		ParametersPackage.eINSTANCE.eClass();
 		ParametersFactory parametersFactory = ParametersFactory.eINSTANCE;
-		
+
 		CompletePattern completePattern = getCompvalAbstract();
-		
-		Element returnElementInReturnGraph = completePattern.getGraph().getReturnElements().get(0);	
+
+		Element element2 = concretize(returnElementType, returnElementAxis, attribute1Name, attribute1Kind,
+				element2Type, element2Axis, attribute2Name, attribute2Kind, attribute3Name, attribute3Kind, parametersFactory,
+				completePattern);
+
+		Comparison comp2 = (Comparison) element2.getPredicates().get(1);
+
+		TextListParam concreteInputValue3 = parametersFactory.createTextListParam();
+		for (String str : values) {
+			concreteInputValue3.getValues().add(str);
+		}
+		((UnknownParameterValue) comp2.getArguments().get(1)).concretize(concreteInputValue3);
+
+		return completePattern;
+	}
+
+	private static Element concretize(String returnElementType, RelationKind returnElementAxis, String attribute1Name,
+			PropertyKind attribute1Kind, String element2Type, RelationKind element2Axis, String attribute2Name, PropertyKind attribute2Kind,
+			String attribute3Name, PropertyKind attribute3Kind, ParametersFactory parametersFactory,
+			CompletePattern completePattern) {
+
+		Element returnElementInReturnGraph = completePattern.getGraph().getElements().get(0);
 		((XmlNavigation) completePattern.getGraph().getRelations().get(0)).getOption().setValue(returnElementAxis);
-		
-		Comparison comparisonReturnElementInReturnGraph = (Comparison) returnElementInReturnGraph.getPredicates().get(0);
+
+		Comparison comparisonReturnElementInReturnGraph = (Comparison) returnElementInReturnGraph.getPredicates()
+				.get(0);
 		TextLiteralParam concreteInputValue = parametersFactory.createTextLiteralParam();
 		concreteInputValue.setValue(returnElementType);
-		((UnknownParameterValue) comparisonReturnElementInReturnGraph.getArguments().get(1)).concretize(concreteInputValue);
-		if(attribute1Kind == PropertyKind.ATTRIBUTE) {
-			((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getAttributeName().setValue(attribute1Name);
+		((UnknownParameterValue) comparisonReturnElementInReturnGraph.getArguments().get(1))
+				.concretize(concreteInputValue);
+		if (attribute1Kind == PropertyKind.ATTRIBUTE) {
+			((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getAttributeName()
+					.setValue(attribute1Name);
 		}
 		((XmlProperty) returnElementInReturnGraph.getProperties().get(0)).getOption().setValue(attribute1Kind);
-		
-		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();		
-		
+
+		Graph graph1 = ((QuantifiedCondition) completePattern.getCondition()).getGraph();
+
 		Element element2 = graph1.getElements().get(1);
-		
+		((XmlNavigation) graph1.getRelations().get(0)).getOption().setValue(element2Axis);
+
 		Comparison comp1 = (Comparison) element2.getPredicates().get(0);
 		TextLiteralParam concreteInputValue2 = parametersFactory.createTextLiteralParam();
 		concreteInputValue2.setValue(element2Type);
 		((UnknownParameterValue) comp1.getArguments().get(1)).concretize(concreteInputValue2);
-		if(attribute2Kind == PropertyKind.ATTRIBUTE) {
+		if (attribute2Kind == PropertyKind.ATTRIBUTE) {
 			((XmlProperty) element2.getProperties().get(0)).getAttributeName().setValue(attribute2Name);
 		}
 		((XmlProperty) element2.getProperties().get(0)).getOption().setValue(attribute2Kind);
-		
-		Comparison comp2 = (Comparison) element2.getPredicates().get(1);
 
-		TextListParam concreteInputValue3 = parametersFactory.createTextListParam();
-		for(String str : values) {
-			concreteInputValue3.getValues().add(str);
-		}
-
-		((UnknownParameterValue) comp2.getArguments().get(1)).concretize(concreteInputValue3);
-		if(attribute3Kind == PropertyKind.ATTRIBUTE) {
+		if (attribute3Kind == PropertyKind.ATTRIBUTE) {
 			((XmlProperty) element2.getProperties().get(1)).getAttributeName().setValue(attribute3Name);
 		}
 		((XmlProperty) element2.getProperties().get(1)).getOption().setValue(attribute3Kind);
-		
-		return completePattern;		
+		return element2;
 	}
-	
+
 	public static CompletePattern getCompvalMidas5230Objekt() {
-		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230", RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Objekt");
+		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230",
+				RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Objekt");
 	}
-	
+
 	public static CompletePattern getCompvalMidas5230Schloss() {
-		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230", RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Schloss");
+		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230",
+				RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Schloss");
 	}
-	
+
 	public static CompletePattern getCompvalMidas5230Print() {
-		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230", RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Print");
+		return getCompvalConcrete("obj", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "5230",
+				RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, "Print");
 	}
-	
+
 	public static CompletePattern getCompvalSetMidas3270() {
-		List<String> values = Arrays.asList("unbekannt","x","y","?");	
-		return getCompvalSetConcrete("kue", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "3270", RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, values);
+		List<String> values = Arrays.asList("unbekannt", "x", "y", "?");
+		return getCompvalSetConcrete("kue", RelationKind.DESCENDANT, "Type", PropertyKind.ATTRIBUTE, "3270",
+				RelationKind.CHILD, "Type", PropertyKind.ATTRIBUTE, "Value", PropertyKind.ATTRIBUTE, values);
+	}
+
+	public static CompletePattern getCompvalLidoEventPlace() {
+		return getCompvalConcrete("lido:lido", RelationKind.DESCENDANT, null, PropertyKind.TAG, "lido:eventPlace",
+				RelationKind.DESCENDANT, null, PropertyKind.TAG, "lido:type", PropertyKind.ATTRIBUTE, "alternative");
+	}
+
+	public static CompletePattern getCompvalLidoObjectWorkTypeObjekt() {
+		return getCompvalThreeElementsConcrete("lido:lido", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:objectWorkType", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:term", RelationKind.DESCENDANT, null, PropertyKind.TAG, 
+				null, PropertyKind.DATA, "Objekt");
 	}
 	
-	public static CompletePattern getCompvalLidoEventPlace() {
-		return getCompvalConcrete("lido:lido", RelationKind.DESCENDANT, null, PropertyKind.TAG, "lido:eventPlace", RelationKind.DESCENDANT, null, PropertyKind.TAG, "lido:type", PropertyKind.ATTRIBUTE, "alternative");
+	public static CompletePattern getCompvalLidoObjectWorkTypeSchloss() {
+		return getCompvalThreeElementsConcrete("lido:lido", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:objectWorkType", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:term", RelationKind.DESCENDANT, null, PropertyKind.TAG, 
+				null, PropertyKind.DATA, "Schloss");
+	}
+	
+	public static CompletePattern getCompvalLidoObjectWorkTypePrint() {
+		return getCompvalThreeElementsConcrete("lido:lido", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:objectWorkType", RelationKind.DESCENDANT, null, PropertyKind.TAG,
+				"lido:term", RelationKind.DESCENDANT, null, PropertyKind.TAG, 
+				null, PropertyKind.DATA, "Print");
 	}
 }
