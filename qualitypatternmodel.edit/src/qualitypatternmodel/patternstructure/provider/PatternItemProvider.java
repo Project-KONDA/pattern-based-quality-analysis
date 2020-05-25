e2f17f6ed8af9e20d7bf7ecd3a4d83b8a8c0f34a
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-05-25 08:13:48 +0200

    Modify genmodel such that Pattern may have children Graph and Condition in the instance editor

--- qualitypatternmodel.edit/src/qualitypatternmodel/patternstructure/provider/PatternItemProvider.java
+++ qualitypatternmodel.edit/src/qualitypatternmodel/patternstructure/provider/PatternItemProvider.java
@@ -9,10 +9,14 @@ import java.util.List;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import qualitypatternmodel.graphstructure.GraphstructureFactory;
 import qualitypatternmodel.patternstructure.Pattern;
+import qualitypatternmodel.patternstructure.PatternstructureFactory;
 import qualitypatternmodel.patternstructure.PatternstructurePackage;
 
 /**
@@ -65,7 +69,7 @@ public class PatternItemProvider extends PatternElementItemProvider {
 				 PatternstructurePackage.Literals.PATTERN__GRAPH,
 				 true,
 				 false,
-				 true,
+				 false,
 				 null,
 				 null,
 				 null));
@@ -87,10 +91,41 @@ public class PatternItemProvider extends PatternElementItemProvider {
 				 PatternstructurePackage.Literals.PATTERN__CONDITION,
 				 true,
 				 false,
-				 true,
+				 false,
 				 null,
 				 null,
 				 null));
+	}
+
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(PatternstructurePackage.Literals.PATTERN__GRAPH);
+			childrenFeatures.add(PatternstructurePackage.Literals.PATTERN__CONDITION);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
 	}
 
 	/**
@@ -118,6 +153,14 @@ public class PatternItemProvider extends PatternElementItemProvider {
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
+
+		switch (notification.getFeatureID(Pattern.class)) {
+			case PatternstructurePackage.PATTERN__GRAPH:
+			case PatternstructurePackage.PATTERN__CONDITION:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
+		super.notifyChanged(notification);
 	}
 
 	/**
@@ -130,6 +173,36 @@ public class PatternItemProvider extends PatternElementItemProvider {
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__GRAPH,
+				 GraphstructureFactory.eINSTANCE.createGraph()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__CONDITION,
+				 PatternstructureFactory.eINSTANCE.createQuantifiedCondition()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__CONDITION,
+				 PatternstructureFactory.eINSTANCE.createFormula()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__CONDITION,
+				 PatternstructureFactory.eINSTANCE.createTrueElement()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__CONDITION,
+				 PatternstructureFactory.eINSTANCE.createNotCondition()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(PatternstructurePackage.Literals.PATTERN__CONDITION,
+				 PatternstructureFactory.eINSTANCE.createCountCondition()));
 	}
 
 }
