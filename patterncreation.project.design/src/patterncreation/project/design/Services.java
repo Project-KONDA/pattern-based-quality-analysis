4d05bbe31d0bc366ce643e586ea82cd16d7b0de3
Lukas <tympel20@mathematik.uni-marburg.de>
2020-06-05 17:36:11 +0200

    New Version

--- patterncreation.project.design/src/patterncreation/project/design/Services.java
+++ patterncreation.project.design/src/patterncreation/project/design/Services.java
@@ -3,14 +3,27 @@ package patterncreation.project.design;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+import org.eclipse.gmf.runtime.diagram.ui.requests.ArrangeRequest;
+import org.eclipse.sirius.common.ui.tools.api.util.EclipseUIUtil;
+import org.eclipse.sirius.viewpoint.SiriusPlugin;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 
+import qualitypatternmodel.graphstructure.Element;
+import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.Condition;
 import qualitypatternmodel.patternstructure.Formula;
 import qualitypatternmodel.patternstructure.NotCondition;
 import qualitypatternmodel.patternstructure.Pattern;
+import qualitypatternmodel.patternstructure.PatternElement;
 import qualitypatternmodel.patternstructure.QuantifiedCondition;
 import qualitypatternmodel.patternstructure.TrueElement;
 
@@ -56,16 +69,9 @@ public class Services {
     	}
     	return !containsTrue;*/
     	int x = 0;
+    	int contentsSize = self.eContents().size();
 		boolean morethanonecontent = false;
-		if (!(self instanceof Formula)) {
-			if (self.eContents().size() > 2) {
-				morethanonecontent = true;
-				x = 1;
-			} else if (self.eContents().size() > 3) {
-				morethanonecontent = true;
-				x = 2;
-			}
-		}else {
+		if (self instanceof Formula) {
 			Formula formula = (Formula) self;
     		Condition condition1 = formula.getCondition1();
     		Condition condition2 = formula.getCondition2();
@@ -73,6 +79,16 @@ public class Services {
     			morethanonecontent = true;
     			x = 3;
     		}
+		}else if(self instanceof QuantifiedCondition) {
+			if(contentsSize >= 3 ) {
+				morethanonecontent = true;
+				x = 2;
+			}
+		}else {
+			if (contentsSize > 2) {
+				morethanonecontent = true;
+				x = 1;
+			}
 		}
     	File file = new File("C:\\Users\\Lukas\\Desktop\\formulacontents.txt");
     	FileWriter writer = null;
@@ -88,7 +104,7 @@ public class Services {
     	return !morethanonecontent;
     }
     
-    public String referenceNameOfNotCondition(EObject self) {
+    public String referenceName(EObject self) {
     	String returnCondition = "x";
     	if(self instanceof Formula) {
     		Formula formula = (Formula) self;
@@ -120,11 +136,22 @@ public class Services {
     	return returnCondition;
     }
     
-    public String formulaConditionOneOrTwo(EObject self) {//muss noch für andere Typen als conditions programmiert werden
+    public String formulaConditionOneOrTwo(EObject self) {//muss noch für andere Typen als notconditions programmiert werden; ist für die Benennung
     	String returnCondition = "";
-    	Formula constr1 = ((Condition)self).getFormula1();
-    	Formula constr2 = ((Condition)self).getFormula2();
-    	String type = "";
+    	if(self instanceof Condition) {
+    		Condition selfCondition = (Condition) self;
+    		Formula formula1 = selfCondition.getFormula1();
+        	Formula formula2 = selfCondition.getFormula2();
+        	if(formula1 == null && formula2 == null) {
+        		
+        	}else if(formula1 == null) {
+        		returnCondition = "2";
+        	}else {
+        		returnCondition = "1";
+        	}
+    	}
+    	
+    	/*String type = "";
     	
     	File file = new File("C:\\Users\\Lukas\\Desktop\\condition12.txt");
     	FileWriter writer = null;
@@ -151,7 +178,137 @@ public class Services {
 			}else if(self instanceof TrueElement) {
 				
 			}
-    	}
+    	}*/
     	return returnCondition;
+    }
+    
+    public String graphName(EObject self) {
+    	EObject container = self.eContainer();
+    	String graphName = "Graph";
+    	if(container instanceof CompletePattern) {
+    		graphName = "ReturnGraph";
+    	}
+    	return graphName;
+    }
+    
+    public String elementName(EObject self) {
+    	String elementName = "Element ";
+    	if(self instanceof Element) {
+    		Element selfElement = (Element) self;
+    		String selfName = selfElement.getName();
+    		if(selfName.equals("First")) {
+    			elementName = "Root";
+    		}else {
+    			elementName = elementName + selfElement.getInternalId();
+    		}
+    	}
+    	return elementName;
+    }
+    
+    /*public EObject rootElement(EObject self) {
+    	EObject rootcontainer = getWurzelContainer(self);
+    	
+    	List<EObject> list1 = new ArrayList<EObject>();
+    	TreeIterator<EObject> iterator = rootcontainer.eAllContents();
+    	
+    	int i = 0;
+    	while(iterator.hasNext()) {
+    		i+=1;*/
+    		/*File file = new File("C:\\Users\\Lukas\\Desktop\\testFile2.txt");
+        	FileWriter writer = null;
+    		try {
+    			file.createNewFile();
+    			writer = new FileWriter(file);
+    			writer.write("Test data\n");
+    			writer.write(String.valueOf(i));
+    			writer.close();
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}*/
+    		/*EObject eo = (EObject) iterator.next();
+
+    		Element2 e2 = null;
+    		Element3 e3 = null;
+    		
+    		if(eo instanceof Element2) {
+    			e2 = (Element2) eo;
+    		}
+    		if(eo instanceof Element3) {
+    			e3 = (Element3) eo;
+    		}
+    		
+    		if(e2!=null) {
+    			if(e2.getNummer()==0) {
+    				Elementundattribut eua = new Elementundattribut();
+    				eua.setAttribut("nummer");
+    				eua.setElement(e2);
+    				list1.add(eua);
+    				
+    				mapfuellen(e2.getName());
+    			}
+    		}
+    		if(e3!=null) {
+    			if(e3.getNummer()==0) {
+    				Elementundattribut eua = new Elementundattribut();
+    				eua.setAttribut("nummer");
+    				eua.setElement(e3);
+    				list1.add(eua);
+    				
+    				mapfuellen(e3.getName());
+    			}
+    		}
+    	}  	
+     	return(list1);
+    }*/
+    
+    public EObject getWurzelContainer(EObject context) {//sucht das Wurzelelement
+    	EObject container1 = null;
+    	EObject container2 = null;
+    	container1 = context.eContainer();
+    	while(container1!=null) {
+    		container2 = container1;
+    		container1 = container1.eContainer();
+    	}
+    	if(container2==null) {//falls context keinen container hat
+    		container2 = context;
+    	}
+    	return container2;
+    }
+    
+    static boolean firstCall = true;
+    public int getSize(EObject self) {
+    	String id = ((PatternElement)self).getId();
+    	File file = new File("C:\\Users\\Lukas\\Desktop\\NeuerOrdner\\"+id+".txt");
+    	FileWriter writer = null;
+		try {
+			file.createNewFile();
+			writer = new FileWriter(file);
+			writer.write("Test\n");
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	int returnint = 10;
+    	if(!firstCall) {
+    		returnint = 100;
+    	}
+    	firstCall = false;
+    	
+    	IEditorPart editor = EclipseUIUtil.getActiveEditor();
+    	List<Object> partsToArrange = new ArrayList<>(1);
+
+    	if (editor instanceof DiagramEditor) {
+    	  DiagramEditor diagramEditor = (DiagramEditor) editor;
+    	  DiagramEditPart diagramEditPart = diagramEditor.getDiagramEditPart();
+    	  
+    	  ArrangeRequest arrangeRequest = new ArrangeRequest(ActionIds.ACTION_ARRANGE_ALL);
+    	  partsToArrange.add(diagramEditPart);
+    	  arrangeRequest.setPartsToArrange(partsToArrange);
+    	  diagramEditPart.performRequest(arrangeRequest);
+    	}
+    	
+    	return 50;
     }
 }
