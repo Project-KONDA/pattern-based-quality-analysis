5da98b4465151c70f32fc14404533ce2093bbf21
kesperar <arno.kesper@uni-marburg.de>
2020-07-15 14:46:34 +0200

    Add titles to numerical references to sections in Artifact document.

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/PatternExecution.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/PatternExecution.java
@@ -34,64 +34,64 @@ public class PatternExecution {
 		// MIDAS
 		String databaseNameMidas = "MIDAS";		
 		
-		execute(EvalMatch.getMatchMidasOb30Child(), databaseNameMidas); // 5.1.1
-		execute(EvalCompval.getCompsetMIDAS3140(), databaseNameMidas); // 5.1.2
-		execute(EvalMandatt.getMandattMidas(), databaseNameMidas); // 5.2.1
-		execute(EvalMandstruc.getMandstrucThreeElementsMidas(), databaseNameMidas); // 5.2.2
-		// 5.2.3
-		execute(EvalCompval.getCompvalSetMidas3270(), databaseNameMidas); // 5.2.4
-		execute(EvalRefint.getRefintMidasWer(), databaseNameMidas); // 5.3
-		execute(EvalUnique.getUniqueMidas(), databaseNameMidas); // 5.4
-		execute(EvalFunc.getFuncMidas(), databaseNameMidas); // 5.5
-		execute(EvalContrel.getContrelMidas(), databaseNameMidas); // 5.6
-		execute(EvalCard.getCardMidasOb30(), databaseNameMidas); // 5.7.1
-		execute(EvalMatch.getMatchMidas3270Imprecise(), databaseNameMidas); // 5.7.2 
-		execute(EvalCompval.getCompvalMidas5230Objekt(), databaseNameMidas); // 5.7.3
-		execute(EvalCompval.getCompvalMidas5230Schloss(), databaseNameMidas); // 5.7.4
-		execute(EvalMatch.getMatchMidas3100Abbreviation(), databaseNameMidas); // 5.7.5
-		execute(EvalMatch.getMatchMidas5060(), databaseNameMidas); // 5.8.1
-		execute(EvalMatch.getMatchMidas5064(), databaseNameMidas); // 5.8.2
-		execute(EvalExdupl.getExactDuplicatesMidasWer(), databaseNameMidas); // 5.9.1
-		execute(EvalAppdupl.getCARD3Midas(), databaseNameMidas); // 5.9.2
-		execute(EvalComp.getCompMidas(), databaseNameMidas); // 5.9.3
-		execute(EvalMatch.getMatchMidas5360(), databaseNameMidas); // 5.10.1
-		execute(EvalCompval.getCompvalMidas5230Print(), databaseNameMidas); // 5.10.2
-		execute(EvalExnex.getExNExMidas(), databaseNameMidas); // 5.10.3
-		// 5.11
-		// 5.12.1
-		// 5.12.2
-		execute(EvalMatch.getMatchMidas3270(), databaseNameMidas); // 5.12.3		
+		execute(EvalMatch.getMatchMidasOb30Child(), databaseNameMidas); // 4.1.1 Wrong Datatype
+		execute(EvalCompval.getCompsetMIDAS3140(), databaseNameMidas); // 4.1.2 Domain Violation
+		execute(EvalMandatt.getMandattMidas(), databaseNameMidas); // 4.2.1 Missing Values
+		execute(EvalMandstruc.getMandstrucThreeElementsMidas(), databaseNameMidas); // 4.2.2 Missing References
+		// 4.2.3 Missing Records
+		execute(EvalCompval.getCompvalSetMidas3270(), databaseNameMidas); // 4.2.4 Dummy Values
+		execute(EvalRefint.getRefintMidasWer(), databaseNameMidas); // 4.3 Referential Integrity Violation
+		execute(EvalUnique.getUniqueMidas(), databaseNameMidas); // 4.4 Unique Value Violation
+		execute(EvalFunc.getFuncMidas(), databaseNameMidas); // 4.5 Violation of a Functional Dependency
+		execute(EvalContrel.getContrelMidas(), databaseNameMidas); // 4.6 Contradictory Relationships
+		execute(EvalCard.getCardMidasOb30(), databaseNameMidas); // 4.7.1 Alternative Possible Values
+		execute(EvalMatch.getMatchMidas3270Imprecise(), databaseNameMidas); // 4.7.2 Imprecise Numerical Values 
+		execute(EvalCompval.getCompvalMidas5230Objekt(), databaseNameMidas); // 4.7.3 Abstract Terms
+		execute(EvalCompval.getCompvalMidas5230Schloss(), databaseNameMidas); // 4.7.4 Ambiguous Values 
+		execute(EvalMatch.getMatchMidas3100Abbreviation(), databaseNameMidas); // 4.7.5 Abbreviations
+		execute(EvalMatch.getMatchMidas5060(), databaseNameMidas); // 4.8.1 Misfielded Values
+		execute(EvalMatch.getMatchMidas5064(), databaseNameMidas); // 4.8.2 Extraneous Data
+		execute(EvalExdupl.getExactDuplicatesMidasWer(), databaseNameMidas); // 4.9.1 Exact Duplicate Records
+		execute(EvalAppdupl.getCARD3Midas(), databaseNameMidas); // 4.9.2 Approximate Duplicate Records
+		execute(EvalComp.getCompMidas(), databaseNameMidas); // 4.9.3 Information Placed in Multiple Locations
+		execute(EvalMatch.getMatchMidas5360(), databaseNameMidas); // 4.10.1 Heterogeneous Measure Units
+		execute(EvalCompval.getCompvalMidas5230Print(), databaseNameMidas); // 4.10.2 Heterogeneous Value Representations
+		execute(EvalExnex.getExNExMidas(), databaseNameMidas); // 4.10.3 Heterogeneous Structural Representations
+		// 4.11 Misspellings
+		// 4.12.1 False Values
+		// 4.12.2 False References
+		execute(EvalMatch.getMatchMidas3270(), databaseNameMidas); // 4.12.3 Doubtful Data	
 		
 		// LIDO
 		String databaseNameLido = "LIDO";	
 		
-		execute(EvalMatch.getMatchLidoMeasurementValue(), databaseNameLido); // 5.1.1
-		execute(EvalCompval.getCompsetLidoGenderActor(), databaseNameLido); // 5.1.2
-		execute(EvalMandatt.getMandattThreeElementsLido(), databaseNameLido); // 5.2.1
-		execute(EvalExnex.getExNExLidoRoleActor(), databaseNameLido); // 5.2.2
-		// 5.2.3
-		execute(EvalCompval.getCompvalSetLidoAppellationValue(), databaseNameLido); // 5.2.4
-		// 5.3
-		execute(EvalUnique.getUniqueLidoLidoRecId(), databaseNameLido); // 5.4
-		execute(EvalFunc.getFuncLido(), databaseNameLido); // 5.5
-		// 5.6
-		execute(EvalCompval.getCompvalLidoEventPlace(), databaseNameLido); // 5.7.1
-		execute(EvalMatch.getMatchLidoEarliestDate(), databaseNameLido); // 5.7.2 
-		execute(EvalCompval.getCompvalLidoObjectWorkTypeObjekt(), databaseNameLido); // 5.7.3
-		execute(EvalCompval.getCompvalLidoObjectWorkTypeSchloss(), databaseNameLido); // 5.7.4
-		execute(EvalMatch.getMatchLidoNameActorSetAbbreviation(), databaseNameLido); // 5.7.5
-		execute(EvalMatch.getMatchLidoMeasurementUnit(), databaseNameLido); // 5.8.1
-		execute(EvalMatch.getMatchLidoNamePlaceSet(), databaseNameLido); // 5.8.2
-		execute(EvalExdupl.getExactDuplicatesLido(), databaseNameLido); // 5.9.1
-		execute(EvalUnique.getUniqueLidoObjectPublishedId(), databaseNameLido); // 5.9.2
-		execute(EvalUnique.getUniqueComplexLidoNameActorSet(), databaseNameLido); // 5.9.3
-		// 5.10.1
-		execute(EvalCompval.getCompvalLidoObjectWorkTypePrint(), databaseNameLido); // 5.10.2
-		execute(EvalCard.getCardLidoActorName(), databaseNameLido); // 5.10.3
-		// 5.11
-		// 5.12.1
-		// 5.12.2
-		execute(EvalMatch.getMatchLidoAppellationValue(), databaseNameLido); // 5.12.3
+		execute(EvalMatch.getMatchLidoMeasurementValue(), databaseNameLido); // 4.1.1 Wrong Datatype
+		execute(EvalCompval.getCompsetLidoGenderActor(), databaseNameLido); // 4.1.2 Domain Violation
+		execute(EvalMandatt.getMandattThreeElementsLido(), databaseNameLido); // 4.2.1 Missing Values
+		execute(EvalExnex.getExNExLidoRoleActor(), databaseNameLido); // 4.2.2 Missing References
+		// 4.2.3 Missing Records
+		execute(EvalCompval.getCompvalSetLidoAppellationValue(), databaseNameLido); // 4.2.4 Dummy Values
+		// 4.3 Referential Integity Violation
+		execute(EvalUnique.getUniqueLidoLidoRecId(), databaseNameLido); // 4.4 Unique Value Violation
+		execute(EvalFunc.getFuncLido(), databaseNameLido); // 4.5 Violation of Functional Dependency
+		// 4.6 Contradictory Relationships
+		execute(EvalCompval.getCompvalLidoEventPlace(), databaseNameLido); // 4.7.1 Alternative Possible Values
+		execute(EvalMatch.getMatchLidoEarliestDate(), databaseNameLido); // 4.7.2 Imprecise Numerical Values
+		execute(EvalCompval.getCompvalLidoObjectWorkTypeObjekt(), databaseNameLido); // 4.7.3 Abstract Terms
+		execute(EvalCompval.getCompvalLidoObjectWorkTypeSchloss(), databaseNameLido); // 4.7.4 Ambiguous Values
+		execute(EvalMatch.getMatchLidoNameActorSetAbbreviation(), databaseNameLido); // 4.7.5 Abbreviations
+		execute(EvalMatch.getMatchLidoMeasurementUnit(), databaseNameLido); // 4.8.1 Misfielded Values
+		execute(EvalMatch.getMatchLidoNamePlaceSet(), databaseNameLido); // 4.8.2 Extraneous Data
+		execute(EvalExdupl.getExactDuplicatesLido(), databaseNameLido); // 4.9.1 Exact Duplicate Records
+		execute(EvalUnique.getUniqueLidoObjectPublishedId(), databaseNameLido); // 4.9.2 Approximate Duplicate records
+		execute(EvalUnique.getUniqueComplexLidoNameActorSet(), databaseNameLido); // 4.9.3 Information Placed in Multiple Locations
+		// 4.10.1 Heterogeneous Measure Units
+		execute(EvalCompval.getCompvalLidoObjectWorkTypePrint(), databaseNameLido); // 4.10.2 Heterogeneous Value Representations
+		execute(EvalCard.getCardLidoActorName(), databaseNameLido); // 4.10.3 Heterogeneous Strucural Representations
+		// 4.11 Misspellings
+		// 4.12.1 False Values
+		// 4.12.2 False References
+		execute(EvalMatch.getMatchLidoAppellationValue(), databaseNameLido); // 4.12.3 Doubtful Data
 		
 		
 		
