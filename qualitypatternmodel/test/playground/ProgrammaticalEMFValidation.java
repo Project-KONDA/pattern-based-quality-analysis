5739cd471e2addb3b1be10cc671432e9357244bd
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-11-05 10:54:11 +0100

    Rename

--- qualitypatternmodel/test/playground/ProgrammaticalEMFValidation.java
+++ qualitypatternmodel/test/playground/ProgrammaticalEMFValidation.java
@@ -16,7 +16,7 @@ import qualitypatternmodel.patternstructure.PatternstructureFactory;
 import qualitypatternmodel.patternstructure.PatternstructurePackage;
 import qualitypatternmodel.testutility.EMFValidationPreparation;
 
-public class ProgrammaticalValidation {
+public class ProgrammaticalEMFValidation {
 	public static void main(String[] args) {	
 
 		EMFValidationPreparation.registerDelegates();
