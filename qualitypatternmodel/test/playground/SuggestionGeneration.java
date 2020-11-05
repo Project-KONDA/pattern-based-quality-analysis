b8ad5464d944430cb8becda5572af279dbfbe398
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-11-05 13:05:11 +0100

    Use demo database for SuggestionGeneration test.

--- qualitypatternmodel/test/playground/SuggestionGeneration.java
+++ qualitypatternmodel/test/playground/SuggestionGeneration.java
@@ -4,6 +4,9 @@ import org.basex.core.BaseXException;
 import org.basex.query.QueryException;
 import org.basex.query.QueryIOException;
 import org.eclipse.emf.common.util.Diagnostic;
+
+import qualitypatternmodel.adaptionxml.PropertyKind;
+import qualitypatternmodel.adaptionxml.XmlProperty;
 import qualitypatternmodel.exceptions.InvalidityException;
 import qualitypatternmodel.exceptions.MissingPatternContainerException;
 import qualitypatternmodel.exceptions.OperatorCycleException;
@@ -11,25 +14,39 @@ import qualitypatternmodel.execution.LocalXmlDataDatabase;
 import qualitypatternmodel.execution.LocalXmlSchemaDatabase;
 import qualitypatternmodel.execution.impl.LocalXmlDataDatabaseImpl;
 import qualitypatternmodel.execution.impl.LocalXmlSchemaDatabaseImpl;
+import qualitypatternmodel.parameters.Parameter;
 import qualitypatternmodel.parameters.TextLiteralParam;
 import qualitypatternmodel.patternstructure.AbstractionLevel;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.testutility.EMFModelLoad;
 import qualitypatternmodel.testutility.EMFValidationPreparation;
+import qualitypatternmodel.translationtests.Test00;
 
 public class SuggestionGeneration {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		
-		 // Loading the existing model
-        EMFModelLoad loader = new EMFModelLoad();
-        CompletePattern completePattern = loader.load("instances/playground/My2.patternstructure");
+		CompletePattern completePattern = Test00.getBasePatternCondConcrete("*");
+		((XmlProperty) completePattern.getGraph().getElements().get(0).getProperties().get(0)).getOption().getOptions().add(PropertyKind.TAG);
+		((XmlProperty) completePattern.getGraph().getElements().get(0).getProperties().get(0)).getOption().setValue(PropertyKind.TAG);
         
-        LocalXmlDataDatabase db = new LocalXmlDataDatabaseImpl("test", "");        
-        LocalXmlSchemaDatabase schema = new LocalXmlSchemaDatabaseImpl("test_schema","C:/Users/Viola Wenz/Documents/Daten/1.1_lido-v1.1-draft.xsd");
-        db.setXmlSchema(schema);
-        db.setNamespace("lido:");
+		try {
+			completePattern.isValid(AbstractionLevel.CONCRETE);
+		} catch (InvalidityException | OperatorCycleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MissingPatternContainerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+        LocalXmlDataDatabase db = new LocalXmlDataDatabaseImpl("test", "demo.data/demo_database.xml");        
+        LocalXmlSchemaDatabase schema = new LocalXmlSchemaDatabaseImpl("test_schema","demo.data/demo_database_schema.xsd");
+        schema.setNamespace("demo:");
+        db.setXmlSchema(schema);                   
+        
         try {
-			schema.init();
+			db.init();
 		} catch (BaseXException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -42,37 +59,18 @@ public class SuggestionGeneration {
 		}
         completePattern.setDatabase(db);
         
-        TextLiteralParam text = (TextLiteralParam) completePattern.getParameterList().getParameters().get(1);
+        System.out.println("> Element Tag Suggestions:\n");
+        TextLiteralParam text = (TextLiteralParam) completePattern.getParameterList().getParameters().get(0);
         for(String s : text.inferElementTagSuggestions()) {
         	System.out.println(s);
         }
-        
-//        XmlNavigation nav = (XmlNavigation) completePattern.getGraph().getRelations().get(1);
-//        for(RelationKind rel : nav.getOption().generateSuggestions()) {
-//        	System.out.println(rel);
-//        }
         
 //        for(String s : db.getElementNames().keySet()) {
 //        	System.out.println(s);
 //        }
 
         
-        try {
-			completePattern.isValid(AbstractionLevel.CONCRETE);
-		} catch (InvalidityException | OperatorCycleException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MissingPatternContainerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			System.out.println(completePattern.generateQuery());
-		} catch (InvalidityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+        
 	}
 	
 
