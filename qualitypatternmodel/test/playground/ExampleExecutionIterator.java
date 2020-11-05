72272eece4684d37c9d1bb078a986d5061406055
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-11-05 10:59:18 +0100

    Use demo database instead of local database

--- qualitypatternmodel/test/playground/ExampleExecutionIterator.java
+++ qualitypatternmodel/test/playground/ExampleExecutionIterator.java
@@ -16,57 +16,26 @@ import qualitypatternmodel.testutility.EMFValidationPreparation;
 
 public class ExampleExecutionIterator {
 	public static void main(String[] args) throws QueryIOException {
-		
-//		EMFValidationPreparation.registerDelegates();
-		
-		 // Loading the existing model
-        EMFModelLoad loader = new EMFModelLoad();
-//        Pattern pattern = loader.load("instances/playground/MatchAppellationUncertain.patternstructure");
-        // TODO: create pattern "MatchAppellationUncertain.patternstructure"
-		
-//		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(pattern);			
-//		
-//		printDiagnostic(diagnostic, "");
-//        
-//        try {
-//			pattern.isValid(false);
-//		} catch (InvalidityException | OperatorCycleException | MissingPatternContainerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} 
+
 		try {
-//			String query = pattern.toXQuery();
-//			String query = "(//*[name()=\"lido:appellationValue\"]";
-			Context context = new Context();		
-//			new CreateDB("DBExample", "D:/Dokumente/LIDO/201902-Update/ddb_20190220/ddb_20190220_1.xml").execute(context);
-			
-			new CreateDB("DBExample", "C:\\Users\\Viola Wenz\\Documents\\Daten\\ddb_20190220\\ddb_20190220\\ddb_20190220_1.xml").execute(context);
-			XQuery xquery = new XQuery("(//*)[1]");
-//			String result = xquery.execute(context);
+			Context context = new Context();					
+			new CreateDB("DBExample", "demo.data/demo_database.xml").execute(context);
 			
 			try(QueryProcessor proc = new QueryProcessor("//*", context)) {
-			      // Store the pointer to the result in an iterator:
-			      Iter iter = proc.iter();
-//			      System.out.println(iter.size());
-			      int i = 0;
-			      for(Item item; (item = iter.next()) != null;) {
+		      Iter iter = proc.iter();
+		      int i = 0;
+		      for(Item item; (item = iter.next()) != null;) {
 //			    	  queryResult.add(item.toJava().toString());
-			    	  i++;
-//			    	  System.out.println(item.serialize().toString());
-//			    	  System.out.println("---------------------");
-			      }
-			      System.out.println(i);
-			    } catch (QueryException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
-//			System.out.println(result);
+		    	  i++;
+		    	  System.out.println(item.serialize().toString());
+		    	  System.out.println("---------------------");
+		      }
+		      System.out.println(i);
+		    } catch (QueryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 		} 
-//		catch (InvalidityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} 
 		catch (BaseXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -74,11 +43,4 @@ public class ExampleExecutionIterator {
 
 	}
 	
-	private static void printDiagnostic(Diagnostic diagnostic, String indent) {
-		System.out.print(indent);
-		System.out.println(diagnostic.getMessage());
-		for (Diagnostic child : diagnostic.getChildren()) {
-			printDiagnostic(child, indent + "  ");
-		}
-	}
 }
