54b979c34fc397497d6aec501e1876c44794f5ab
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-11-05 11:01:31 +0100

    Rename

--- qualitypatternmodel/test/playground/ReadFile.java
+++ qualitypatternmodel/test/playground/ReadFile.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class FileReadTest {
+public class ReadFile {
 	public static void main(String[] args) {
 		String s;
 		try {
