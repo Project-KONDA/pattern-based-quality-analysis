5205a7deebc401c9886b7d4ddb360a4770f90e1f
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2020-11-05 10:53:38 +0100

    Remove doc folder

--- qualitypatternmodel/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"qualitypatternmodel.graphstructure","l":"SingleElement"},{"p":"qualitypatternmodel.graphstructure.impl","l":"SingleElementImpl"}]
