68169f7c2cca1e6b3d752ed1a24214cfe2b83d10
Lukas <tympel20@mathematik.uni-marburg.de>
2020-11-06 11:49:19 +0100

    Delete unused test files

--- patterncreation.project.design/srctest/patterncreation/project/design/test/DerivedSiriusDiagramTestCase.java
+++
@@ -1,7 +0,0 @@
-package patterncreation.project.design.test;
-
-import org.eclipse.sirius.tests.support.api.SiriusDiagramTestCase;
-
-public class DerivedSiriusDiagramTestCase extends SiriusDiagramTestCase{
-
-}
