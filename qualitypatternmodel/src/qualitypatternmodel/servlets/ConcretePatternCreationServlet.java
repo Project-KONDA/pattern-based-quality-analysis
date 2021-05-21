6a2ee5149853f69e2c78f892f324240868d35c7e
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-05-21 11:21:00 +0200

    Send error if concrete pattern with given name exists already

--- qualitypatternmodel/src/qualitypatternmodel/servlets/ConcretePatternCreationServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/ConcretePatternCreationServlet.java
@@ -20,17 +20,24 @@ public class ConcretePatternCreationServlet extends HttpServlet {
 		String requestUrl = request.getRequestURI();
 		String abstractPatternName = requestUrl.substring("/qualitypatternmodel/abstract-patterns/".length());
 		String concretePatternName = request.getParameter("name");
-		String path = "../../abstract-patterns/" + abstractPatternName + ".patternstructure";		
-		URL abstractPatternUrl = getClass().getClassLoader().getResource(path);	
+		String abstractPatternPath = "../../abstract-patterns/" + abstractPatternName + ".patternstructure";		
+		URL abstractPatternUrl = getClass().getClassLoader().getResource(abstractPatternPath);	
 		if(abstractPatternUrl != null) {
 			CompletePattern pattern = EMFModelLoad.loadCompletePattern(abstractPatternUrl.toString());		
 			if(pattern != null) {
 				try {
 					pattern.setName(concretePatternName);
-					URL concretePatternFolderUrl = getClass().getClassLoader().getResource("../../concrete-patterns/");
-					// TODO: check if file exists
-					EMFModelSave.exportToFile(pattern, concretePatternFolderUrl.toString() + concretePatternName, "patternstructure");
-					response.getOutputStream().println("Successfully createed concrete pattern with name '" + concretePatternName + "' from abstract pattern '" + abstractPatternName + "'.");
+					String concretePatternFolderPath = "../../concrete-patterns/";
+					String concretePatternPath = concretePatternFolderPath + concretePatternName + ".patternstructure";		
+					URL concretePatternURL = getClass().getClassLoader().getResource(concretePatternPath);		
+					if(concretePatternURL == null) {
+						URL concretePatternFolderUrl = getClass().getClassLoader().getResource(concretePatternFolderPath);
+						EMFModelSave.exportToFile(pattern, concretePatternFolderUrl.toString() + concretePatternName, "patternstructure");
+						response.getOutputStream().println("Successfully created concrete pattern with name '" + concretePatternName + "' from abstract pattern '" + abstractPatternName + "'.");
+					} else {
+						response.sendError(400);
+						response.getOutputStream().println("{ \"error\": \"Pattern name already used\"}");						
+					}					
 				} catch (IOException e) {
 					response.sendError(500);
 					response.getOutputStream().println("{ \"error\": \"Saving concrete pattern failed\"}");
