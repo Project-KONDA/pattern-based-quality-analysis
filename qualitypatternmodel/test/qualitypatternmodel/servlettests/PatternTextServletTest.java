252b844c5eba0f2ffc7901dcdc1d5176cd43cb0b
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-05-25 15:22:39 +0200

    Add validation of http response against JSON schema

--- qualitypatternmodel/test/qualitypatternmodel/servlettests/PatternTextServletTest.java
+++ qualitypatternmodel/test/qualitypatternmodel/servlettests/PatternTextServletTest.java
@@ -1,26 +1,42 @@
 package qualitypatternmodel.servlettests;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Scanner;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.junit.Test;
 
 public class PatternTextServletTest {
 	
-	public static void main(String[] args) throws IOException {
-		doGetTest();
-	}
-
-	public static void doGetTest() throws IOException {
+	@Test
+	public void doGetTest() throws IOException {
 		HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8081/qualitypatternmodel/concrete-patterns/text/card_concrete/test_text").openConnection();
 		connection.setRequestMethod("GET");
 				
 		int responseCode = connection.getResponseCode();
 		
 		String result = ServletTestsUtil.getResult(connection);		
-		ServletTestsUtil.printResult(connection, responseCode, result);
+//		ServletTestsUtil.printResult(connection, responseCode, result);
+		
+		try {
+			JSONObject jsonObject = new JSONObject(result);			
+			File initialFile = new File("patterntext.schema.json");
+		    InputStream inputStream = new FileInputStream(initialFile);
+			org.json.JSONObject rawSchema = new org.json.JSONObject(new JSONTokener(inputStream));
+			Schema schema = SchemaLoader.load(rawSchema);
+			schema.validate(jsonObject);           	  
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  	  
 
 	}
 	
