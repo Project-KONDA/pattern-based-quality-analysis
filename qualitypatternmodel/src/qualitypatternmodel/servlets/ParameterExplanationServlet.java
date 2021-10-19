44584c5f2eb9efe396fbd161f3cd9beb34a06a60
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-19 11:55:18 +0200

    Add servlet which provides parameter explanations

---
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/ParameterExplanationServlet.java
@@ -0,0 +1,40 @@
+package qualitypatternmodel.servlets;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.emf.common.util.EList;
+
+import qualitypatternmodel.patternstructure.CompletePattern;
+import qualitypatternmodel.textrepresentation.PatternText;
+import qualitypatternmodel.utility.Constants;
+import qualitypatternmodel.utility.EMFModelLoad;
+
+public class ParameterExplanationServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {		
+		String result = "["
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_TEXT+"\", \"Explanation\":\"An arbitrary string value\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_TEXT_LIST+"\", \"Explanation\":\"A list of string values\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_NUMBER+"\", \"Explanation\":\"A decimal number\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_BOOLEAN+"\", \"Explanation\":\"true or false\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_DATE+"\", \"Explanation\":\"A date consisting of day, month and year\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_TIME+"\", \"Explanation\":\"A time consisting of hours and minutes\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_DATE_TIME+"\", \"Explanation\":\"A combination of a date and a time\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_RELATION+"\", \"Explanation\":\"A relation between two XML elements in a hierarchy\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_COMPARISON+"\", \"Explanation\":\"A comparison operator two compare to values\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_PROPERTY+"\", \"Explanation\":\"A property of an XML element\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_TYPE+"\", \"Explanation\":\"A type of a comparison\"}, "
+				+ "{\"Parameter\":\""+Constants.PARAMETER_TYPE_UNTYPED+"\", \"Explanation\":\"A value of any type\"},"
+				+ "]";
+		response.getOutputStream().println(result);		
+	}
+	
+}
