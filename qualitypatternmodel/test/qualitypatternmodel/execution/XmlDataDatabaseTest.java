ecaa6b2ac788bfa97754ed16bdd6214206f94493
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-20 14:19:55 +0200

    Add tests

---
+++ qualitypatternmodel/test/qualitypatternmodel/execution/XmlDataDatabaseTest.java
@@ -0,0 +1,38 @@
+package qualitypatternmodel.execution;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.basex.query.QueryException;
+import org.eclipse.emf.common.util.EList;
+import org.junit.Before;
+import org.junit.Test;
+
+import qualitypatternmodel.exceptions.InvalidityException;
+import qualitypatternmodel.execution.impl.ServerXmlDataDatabaseImpl;
+
+public class XmlDataDatabaseTest {
+	ServerXmlDataDatabase db;
+	
+	@Before
+	public void before() throws IOException {
+		db = new ServerXmlDataDatabaseImpl("ddb_20190606", "localhost", 1984, "admin", "admin");
+
+	}
+	
+	@Test
+	public void findXSDURLTest() throws IOException, QueryException, InvalidityException {
+		String url = db.findXSDURL();
+		assertEquals(url, "http://www.lido-schema.org/schema/v1.0/lido-v1.0.xsd");
+	}
+	
+	@Test
+	public void createSchemaDatabaseFromReferencedSchemaTest() throws IOException, QueryException, InvalidityException {
+		LocalXmlSchemaDatabase schemaDb = db.createSchemaDatabaseFromReferencedSchema();
+		EList<String> wholeDb = schemaDb.execute("/*");
+		assertTrue(wholeDb.size() > 0);
+	}
+
+}
