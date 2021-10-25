3e4e510c9c17ffaff33f00615cfcd988011f0117
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-25 11:01:27 +0200

    Fix resolving of reference between database and pattern

--- qualitypatternmodel/src/qualitypatternmodel/servlets/DatabaseGettingServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/DatabaseGettingServlet.java
@@ -22,6 +22,7 @@ import qualitypatternmodel.execution.LocalXmlSchemaDatabase;
 import qualitypatternmodel.execution.ServerXmlDataDatabase;
 import qualitypatternmodel.execution.XmlDataDatabase;
 import qualitypatternmodel.execution.XmlDatabase;
+import qualitypatternmodel.execution.impl.DatabasesImpl;
 import qualitypatternmodel.execution.impl.ServerXmlDataDatabaseImpl;
 import qualitypatternmodel.parameters.BooleanParam;
 import qualitypatternmodel.parameters.DateParam;
@@ -54,27 +55,37 @@ public class DatabaseGettingServlet extends HttpServlet {
 		URL folderURLPattern = getClass().getClassLoader().getResource(Util.CONCRETE_PATTERNS_PATH);
 		URL fileURLPattern = getClass().getClassLoader().getResource(filePathPattern);	
 		
-		if(fileURLPattern != null && folderURLPattern != null) {
-			CompletePattern pattern = EMFModelLoad.loadCompletePattern(fileURLPattern.toString());
-			Database database = pattern.getDatabase();
-			if(database instanceof ServerXmlDataDatabase) {			
-				ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) database;
-				String result = "";
-				result += "{\"LocalName\":";
-				result += "\"" + serverDatabase.getLocalName() + "\", ";
-				result += "\"Host\":";
-				result += "\"" + serverDatabase.getBaseXClient().getHost() + "\", ";
-				result += "\"Port\":";		
-				result += "\"" + Integer.toString(serverDatabase.getBaseXClient().getPort()) + "\", ";
-				result += "\"Name\":";		
-				result += "\"" + serverDatabase.getName() + "\", ";							
-				result += "}";
+		if(fileURLPattern != null && folderURLPattern != null) {			
 			
-				response.getOutputStream().println(result);
-			} else {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Getting database failed.\"}");
+			String filePathDb = Util.DATABASES_PATH + Util.DATABASES_NAME + ".execution";	
+			URL folderURLDb = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
+			URL fileURLDb = getClass().getClassLoader().getResource(filePathDb);		
+							
+			if(fileURLDb != null && folderURLDb != null) {			
+				DatabasesImpl.getInstance().clear();
+				CompletePattern pattern = EMFModelLoad.loadCompletePatternAndDatabase(fileURLPattern.toString(), fileURLDb.toString());				
+				Database database = pattern.getDatabase();				
+				
+				if(database instanceof ServerXmlDataDatabase) {			
+					ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) database;
+					String result = "";
+					result += "{\"LocalName\":";
+					result += "\"" + serverDatabase.getLocalName() + "\", ";
+					result += "\"Host\":";
+					result += "\"" + serverDatabase.getBaseXClient().getHost() + "\", ";
+					result += "\"Port\":";		
+					result += "\"" + Integer.toString(serverDatabase.getBaseXClient().getPort()) + "\", ";
+					result += "\"Name\":";		
+					result += "\"" + serverDatabase.getName() + "\", ";							
+					result += "}";
+				
+					response.getOutputStream().println(result);
+				} else {
+					response.sendError(404);
+					response.getOutputStream().println("{ \"error\": \"Getting database failed.\"}");
+				}
 			}
+	
 			
 		} else {
 			response.sendError(404);
--- qualitypatternmodel/src/qualitypatternmodel/utility/EMFModelLoad.java
+++ qualitypatternmodel/src/qualitypatternmodel/utility/EMFModelLoad.java
@@ -2,14 +2,21 @@ package qualitypatternmodel.utility;
 
 import java.util.Map;
 
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import qualitypatternmodel.execution.Databases;
 import qualitypatternmodel.execution.ExecutionPackage;
+import qualitypatternmodel.execution.ServerXmlDataDatabase;
+import qualitypatternmodel.execution.XmlDatabase;
+import qualitypatternmodel.execution.impl.DatabasesImpl;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.PatternstructurePackage;
 
@@ -38,6 +45,60 @@ public class EMFModelLoad {
 		}
 	}
 
+	public static CompletePattern loadCompletePatternAndDatabase(String patternPath, String dbPath) {
+		ExecutionPackage.eINSTANCE.eClass();
+		String path = dbPath;
+		String fileEnding = "execution";
+		// Register the XMI resource factory for the .patternstructure extension
+        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m = reg.getExtensionToFactoryMap();
+        m.put(fileEnding, new XMIResourceFactoryImpl());
+
+        // Obtain a new resource set
+        ResourceSet resSet = new ResourceSetImpl();
+
+        // Get the resource
+        Resource resource = resSet.getResource(URI
+                .createURI(path), true);
+		if(resource.getContents().get(0) instanceof Databases) {
+			Databases db = (Databases) resource.getContents().get(0);	         
+		} else {
+			return null;
+		}
+		
+		EList<XmlDatabase> correctList = new BasicEList<XmlDatabase>();
+		correctList.addAll(DatabasesImpl.getInstance().getXmlDatabases());
+				
+		PatternstructurePackage.eINSTANCE.eClass();
+		path = patternPath;
+		fileEnding = "patternstructure";
+		// Register the XMI resource factory for the .patternstructure extension
+        Resource.Factory.Registry reg2 = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m2 = reg2.getExtensionToFactoryMap();
+        m2.put(fileEnding, new XMIResourceFactoryImpl());
+
+        // Get the resource
+        Resource resource2 = resSet.getResource(URI
+                .createURI(path), true);
+       
+		EList<XmlDatabase> copy = new BasicEList<XmlDatabase>();
+		copy.addAll(DatabasesImpl.getInstance().getXmlDatabases());
+		
+		for(XmlDatabase db : copy) {
+			if(db.eIsProxy()) {			
+				DatabasesImpl.getInstance().getXmlDatabases().remove(db);
+			}
+		}
+		
+		assert(correctList.containsAll(DatabasesImpl.getInstance().getXmlDatabases()) && DatabasesImpl.getInstance().getXmlDatabases().containsAll(correctList));
+
+		if(resource2.getContents().get(0) instanceof CompletePattern) {
+			return (CompletePattern) resource2.getContents().get(0);	  
+		} else {
+			return null;
+		}
+	}
+	
 	private static Resource load(String path, String fileEnding) {
 		
         // Register the XMI resource factory for the .patternstructure extension
