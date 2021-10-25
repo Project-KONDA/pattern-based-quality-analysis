0f4eb1bcf6a464b57a0409f32d6e0751ff203b5d
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-25 12:15:19 +0200

    Delete reference to deleted database in patterns

--- qualitypatternmodel/src/qualitypatternmodel/servlets/DatabaseDeletionServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/DatabaseDeletionServlet.java
@@ -12,10 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 
 import qualitypatternmodel.execution.Databases;
 import qualitypatternmodel.execution.ServerXmlDataDatabase;
 import qualitypatternmodel.execution.XmlDataDatabase;
+import qualitypatternmodel.execution.XmlDatabase;
 import qualitypatternmodel.execution.impl.DatabasesImpl;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.utility.EMFModelLoad;
@@ -33,28 +35,68 @@ public class DatabaseDeletionServlet extends HttpServlet {
 		URL folderURL = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
 		URL fileURL = getClass().getClassLoader().getResource(filePath);		
 						
-		if(fileURL != null && folderURL != null) {
-			Databases databasesContainer = EMFModelLoad.loadDatabases(fileURL.toString());
-			boolean found = false;
-			EList<XmlDataDatabase> copy = new BasicEList<XmlDataDatabase>();
-			copy.addAll(databasesContainer.getXmlDatabases());
-		
-			for (XmlDataDatabase db: copy) {
-				if(db instanceof ServerXmlDataDatabase) {
-					ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) db;
-					if(serverDatabase.getLocalName().equals(localName)) {
-						serverDatabase.delete();
-						found = true;
-					}
-				}						
-			}		
-			if(found) {
-				// TODO: save patterns?
-				EMFModelSave.exportToFile(databasesContainer, folderURL.toString() + Util.DATABASES_NAME, "execution");
-				response.getOutputStream().println("Successfully deleted local database '" + localName + "'.");	
-			} else {
+		if(fileURL != null && folderURL != null) {			
+			
+			try {
+				List<String> fileNames = Util.getListOfFileNamesInFolder(Util.CONCRETE_PATTERNS_PATH, getClass());
+				EList<CompletePattern> patterns = new BasicEList<CompletePattern>();
+				DatabasesImpl.getInstance().clear();
+				
+				ResourceSet set = EMFModelLoad.loadDatabasesInResourceSet(fileURL.toString());			
+				
+				for (String name: fileNames) {				
+					String filePathPattern = Util.CONCRETE_PATTERNS_PATH + name + ".patternstructure";	
+					URL folderURLPattern = getClass().getClassLoader().getResource(Util.CONCRETE_PATTERNS_PATH);
+					URL fileURLPattern = getClass().getClassLoader().getResource(filePathPattern);						
+					
+					if(fileURLPattern != null && folderURLPattern != null) {						
+						CompletePattern pattern = EMFModelLoad.loadCompletePatternInResourceSet(fileURLPattern.toString(), set);
+						if(pattern.getDatabase() != null) {
+							if(pattern.getDatabase() instanceof ServerXmlDataDatabase) {
+								ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) pattern.getDatabase();
+								if(serverDatabase.getLocalName().equals(localName)) {
+									pattern.setDatabase(null);
+								}
+							}
+						}						
+						patterns.add(pattern);					
+					}				
+				}
+				boolean found = false;
+				EList<XmlDataDatabase> copy = new BasicEList<XmlDataDatabase>();
+				copy.addAll(DatabasesImpl.getInstance().getXmlDatabases());
+				for (XmlDataDatabase db: copy) {					
+					if(db instanceof ServerXmlDataDatabase) {
+						ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) db;
+						if(serverDatabase.getLocalName().equals(localName)) {
+							serverDatabase.delete();
+							found = true;
+						}
+					}						
+				}		
+				if(found) {
+					int i = 0;
+					for (String name: fileNames) {				
+						String filePathPattern = Util.CONCRETE_PATTERNS_PATH + name + ".patternstructure";	
+						URL folderURLPattern = getClass().getClassLoader().getResource(Util.CONCRETE_PATTERNS_PATH);
+						URL fileURLPattern = getClass().getClassLoader().getResource(filePathPattern);		
+						
+						if(fileURLPattern != null && folderURLPattern != null) {
+							CompletePattern pattern = patterns.get(i);
+							EMFModelSave.exportToFile(pattern, folderURLPattern.toString() + name, "patternstructure");					
+						}			
+						i++;
+					}					
+					EMFModelSave.exportToFile(DatabasesImpl.getInstance(), folderURL.toString() + Util.DATABASES_NAME, "execution");
+					response.getOutputStream().println("Successfully deleted local database '" + localName + "'.");	
+				} else {
+					response.sendError(404);
+					response.getOutputStream().println("{ \"error\": \"Could not find data base with local name '" + localName + "'.\"}");
+				}
+				
+			} catch (URISyntaxException e) {
 				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Could not find data base with local name '" + localName + "'.\"}");
+				response.getOutputStream().println("{ \"error\": \"Loading concrete pattern folder failed.\"}");
 			}
 			
 			
--- qualitypatternmodel/src/qualitypatternmodel/utility/EMFModelLoad.java
+++ qualitypatternmodel/src/qualitypatternmodel/utility/EMFModelLoad.java
@@ -44,6 +44,66 @@ public class EMFModelLoad {
 			return null;
 		}
 	}
+	
+	public static ResourceSet loadDatabasesInResourceSet(String dbPath) {
+		
+		ExecutionPackage.eINSTANCE.eClass();
+		String fileEnding = "execution";
+		// Register the XMI resource factory for the .patternstructure extension
+        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m = reg.getExtensionToFactoryMap();
+        m.put(fileEnding, new XMIResourceFactoryImpl());
+
+        // Obtain a new resource set
+        ResourceSet resSet = new ResourceSetImpl();
+
+        // Get the resource
+        Resource resource = resSet.getResource(URI
+                .createURI(dbPath), true);
+		if(resource.getContents().get(0) instanceof Databases) {
+			Databases db = (Databases) resource.getContents().get(0);	         
+		} else {
+			return null;
+		}
+		
+		return resSet;
+		
+	}
+	
+	public static CompletePattern loadCompletePatternInResourceSet(String patternPath, ResourceSet resSet) {
+		
+		EList<XmlDatabase> correctList = new BasicEList<XmlDatabase>();
+		correctList.addAll(DatabasesImpl.getInstance().getXmlDatabases());
+				
+		PatternstructurePackage.eINSTANCE.eClass();
+		String path = patternPath;
+		String fileEnding = "patternstructure";
+		// Register the XMI resource factory for the .patternstructure extension
+        Resource.Factory.Registry reg2 = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m2 = reg2.getExtensionToFactoryMap();
+        m2.put(fileEnding, new XMIResourceFactoryImpl());
+
+        // Get the resource
+        Resource resource2 = resSet.getResource(URI
+                .createURI(path), true);
+       
+		EList<XmlDatabase> copy = new BasicEList<XmlDatabase>();
+		copy.addAll(DatabasesImpl.getInstance().getXmlDatabases());
+		
+		for(XmlDatabase db : copy) {
+			if(db.eIsProxy()) {			
+				DatabasesImpl.getInstance().getXmlDatabases().remove(db);
+			}
+		}
+		
+		assert(correctList.containsAll(DatabasesImpl.getInstance().getXmlDatabases()) && DatabasesImpl.getInstance().getXmlDatabases().containsAll(correctList));
+
+		if(resource2.getContents().get(0) instanceof CompletePattern) {
+			return (CompletePattern) resource2.getContents().get(0);	  
+		} else {
+			return null;
+		}
+	}
 
 	public static CompletePattern loadCompletePatternAndDatabase(String patternPath, String dbPath) {
 		ExecutionPackage.eINSTANCE.eClass();
