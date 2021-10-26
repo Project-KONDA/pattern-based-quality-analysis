0d1405662738a4dc51ba53085e59bf51757b6ac3
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-26 13:34:33 +0200

    Rework card demo pattern text flexible variant

--- qualitypatternmodel/demo/qualitypatternmodel/demo/DemoPatternTexts.java
+++ qualitypatternmodel/demo/qualitypatternmodel/demo/DemoPatternTexts.java
@@ -39,6 +39,7 @@ import qualitypatternmodel.utility.EMFModelSave;
 
 public class DemoPatternTexts {
 	
+	private static final String CARD_FLEXIBLE = "flexible";
 	private static final String CARD_PARENT_WITH_MORE_THAN_ONE_CHILD = "parent_with_more_than_one_child";
 
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException, IOException {
@@ -229,7 +230,7 @@ public class DemoPatternTexts {
 	
 	public static void addTextualRepresentationCompPattern0WithoutExamples(CompletePattern completePattern) {
 		PatternText patternText = TextrepresentationFactory.eINSTANCE.createPatternText();
-		patternText.setName("flexible");
+		patternText.setName(CARD_FLEXIBLE);
 		completePattern.getText().add(patternText);
 		
 		// Is there an element with <property> <value> that has a <relation> whose <property> is equal to <value> and whose <property> is equal to  <value>?
@@ -333,7 +334,7 @@ public class DemoPatternTexts {
 	
 	public static void addTextualRepresentationCompPattern0(CompletePattern completePattern) {
 		PatternText patternText = TextrepresentationFactory.eINSTANCE.createPatternText();
-		patternText.setName("flexible");
+		patternText.setName(CARD_FLEXIBLE);
 		completePattern.getText().add(patternText);
 		
 		// Is there an element with <property> <value> that has a <relation> whose <property> is equal to <value> and whose <property> is equal to  <value>?
@@ -673,22 +674,27 @@ public class DemoPatternTexts {
 
 	public static void addTextualRepresentationCardPattern0(CompletePattern completePattern) {
 		PatternText patternText = TextrepresentationFactory.eINSTANCE.createPatternText();
-		patternText.setName("flexible");
+		patternText.setName(CARD_FLEXIBLE);
 		completePattern.getText().add(patternText);
 		
-		// Is there an element with <property> = <value> that has <comp> <number> <relation> whose <property> is equal to <value>?
+		// Is there an element with <property_1> equal to <value_1> that has <comparison_operator> <number> <relation_type> element(s) whose <property_2> is equal to <value_2>?
 		
-		// example: Is there an element with 'attribute job' = 'painter' that has '>' '2' 'child' whose 'data' is equal to 'painting'?
-		
+		// example: Is there an element with 'TAG' equal to 'building' that has '>=' '3' 'descendant' element(s) whose 'DATA' is equal to 'unknown'?
 		
 		TextFragment text0 = TextrepresentationFactory.eINSTANCE.createTextFragment();
 		text0.setText("Is there an element with");
 		TextFragment text1 = TextrepresentationFactory.eINSTANCE.createTextFragment();
 		text1.setText("that has");
 		TextFragment text2 = TextrepresentationFactory.eINSTANCE.createTextFragment();
-		text2.setText("whose");
+		text2.setText("element(s) whose");
 		TextFragment text3 = TextrepresentationFactory.eINSTANCE.createTextFragment();
 		text3.setText("is equal to");
+		
+		TextFragment text4 = TextrepresentationFactory.eINSTANCE.createTextFragment();
+		text4.setText("equal to");
+		
+		TextFragment text5 = TextrepresentationFactory.eINSTANCE.createTextFragment();
+		text5.setText("?");
 		
 		
 		// Context graph of pattern:
@@ -698,11 +704,11 @@ public class DemoPatternTexts {
 		XmlElement element0 = (XmlElement) completePattern.getGraph().getElements().get(0);
 		XmlProperty property0 = (XmlProperty) element0.getProperties().get(0);
 		param0.setParameter(property0.getOption());
-		param0.setExampleValue(PropertyKind.ATTRIBUTE.getLiteral());
+		param0.setName("property_1");
+		param0.setExampleValue(PropertyKind.TAG.getLiteral());
 		
 		ParameterFragment param1 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		param1.setParameter(property0.getAttributeName());
-		param1.setExampleValue("job");
 		
 //		// <comp>
 //		ParameterFragment param11 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
@@ -714,7 +720,8 @@ public class DemoPatternTexts {
 		ParameterFragment param2 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		UntypedParameterValue value0 = (UntypedParameterValue) comp0.getArgument2();
 		param2.setParameter(value0);
-		param2.setExampleValue("painter");
+		param2.setName("value_1");
+		param2.setExampleValue("building");
 				
 		
 		// First-order logic condition of pattern:
@@ -725,12 +732,14 @@ public class DemoPatternTexts {
 		// <comp>
 		ParameterFragment param3 = TextrepresentationFactory.eINSTANCE.createParameterFragment();		
 		param3.setParameter(countCondition.getOption());
-		param3.setExampleValue(ComparisonOperator.GREATER.getLiteral());
+		param3.setName("comparison__operator");
+		param3.setExampleValue(ComparisonOperator.GREATEROREQUAL.getLiteral());
 		
 		// <number>
 		ParameterFragment param4 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		param4.setParameter(((NumberElement) countCondition.getArgument2()).getNumberParam());
-		param4.setExampleValue("2");
+		param4.setName("number");
+		param4.setExampleValue("3");
 		
 		
 		// Graph of inner pattern:
@@ -739,14 +748,16 @@ public class DemoPatternTexts {
 		ParameterFragment param5 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		XmlNavigation navigationElement0Element1 = (XmlNavigation) countPattern.getGraph().getRelations().get(0);
 		param5.setParameter(navigationElement0Element1.getOption());
-		param5.setExampleValue(RelationKind.CHILD.getLiteral());
+		param5.setName("relation_type");
+		param5.setExampleValue(RelationKind.DESCENDANT.getLiteral());
 		
 		// <property>
 		ParameterFragment param6 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		XmlElement element1 = (XmlElement) countPattern.getGraph().getElements().get(1);
 		XmlProperty property1 = (XmlProperty) element1.getProperties().get(0);
 		param6.setParameter(property1.getOption());
-		param6.setExampleValue(PropertyKind.DATA.getLiteral());
+		param6.setName("property_2");
+		param6.setExampleValue(PropertyKind.DATA.getLiteral());		
 		
 		ParameterFragment param7 = TextrepresentationFactory.eINSTANCE.createParameterFragment();
 		param7.setParameter(property1.getAttributeName());
@@ -756,24 +767,24 @@ public class DemoPatternTexts {
 		Comparison comp1 = (Comparison) countPattern.getGraph().getOperatorList().getOperators().get(0);
 		UntypedParameterValue value1 = (UntypedParameterValue) comp1.getArgument2();
 		param8.setParameter(value1);
-		param8.setExampleValue("painting");
-		
-		
-		// Is there an element with <property> <value> that has <comp> <number> <relation> whose <property> is equal to <value>?
+		param8.setName("value_2");
+		param8.setExampleValue("unknown");
 		
 		patternText.getFragments().add(text0);  // Is there an element with
-		patternText.getFragments().add(param0); // <property>
+		patternText.getFragments().add(param0); // <property_1>
 		patternText.getFragments().add(param1);
-		patternText.getFragments().add(param2); // <value>		
+		patternText.getFragments().add(text4);  // equal to
+		patternText.getFragments().add(param2); // <value_1>		
 		patternText.getFragments().add(text1);  // that has
-		patternText.getFragments().add(param3); // <comp>
+		patternText.getFragments().add(param3); // <comparison_operator>
 		patternText.getFragments().add(param4); // <number>
-		patternText.getFragments().add(param5); // <relation>
-		patternText.getFragments().add(text2);  // whose
-		patternText.getFragments().add(param6); // <property>
+		patternText.getFragments().add(param5); // <relation_type>
+		patternText.getFragments().add(text2);  // element(s) whose
+		patternText.getFragments().add(param6); // <property_2>
 		patternText.getFragments().add(param7);
 		patternText.getFragments().add(text3);  // is equal to
-		patternText.getFragments().add(param8); // <value>
+		patternText.getFragments().add(param8); // <value_2>
+		patternText.getFragments().add(text5);  // ?
 	
 	}
 	
