92d1861b09da3f5abc752f69d28940bfab43d4ce
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-26 10:39:05 +0200

    Access databases file directly

--- qualitypatternmodel/src/qualitypatternmodel/servlets/DatabasesListServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/DatabasesListServlet.java
@@ -22,55 +22,36 @@ public class DatabasesListServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-		try {
-			List<String> fileNames = Util.getListOfFileNamesInFolder(Util.DATABASES_PATH, getClass());	
+		String filePath = Util.DATABASES_PATH + Util.DATABASES_NAME + ".execution";	
+		URL folderURL = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
+		URL fileURL = getClass().getClassLoader().getResource(filePath);		
+						
+		if(fileURL != null && folderURL != null) {					
 			
+			DatabasesImpl.getInstance().clear();					
+			Databases databasesContainer = EMFModelLoad.loadDatabases(fileURL.toString());		
 			
-			if(fileNames == null) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Loading database folder failed\"}");
-			} else if(fileNames.isEmpty()) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"No databases found\"}");
-			} else if(fileNames.size() > 1) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Too many database containers found\"}");
-			} else {
-				String fileName = fileNames.get(0);
-				String result = "[";
-				
-				String filePath = Util.DATABASES_PATH + fileName + ".execution";	
-				URL folderURL = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
-				URL fileURL = getClass().getClassLoader().getResource(filePath);		
-								
-				if(fileURL != null && folderURL != null) {					
-					
-					DatabasesImpl.getInstance().clear();					
-					Databases databasesContainer = EMFModelLoad.loadDatabases(fileURL.toString());					
-				
-					for (XmlDataDatabase db: databasesContainer.getXmlDatabases()) {
-						if(db instanceof ServerXmlDataDatabase) {
-							ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) db;
-							result += "{\"LocalName\":";
-							result += "\"" + serverDatabase.getLocalName() + "\", ";
-							result += "\"Host\":";
-							result += "\"" + serverDatabase.getBaseXClient().getHost() + "\", ";
-							result += "\"Port\":";		
-							result += "\"" + Integer.toString(serverDatabase.getBaseXClient().getPort()) + "\", ";
-							result += "\"Name\":";		
-							result += "\"" + serverDatabase.getName() + "\", ";							
-							result += "}, ";
-						}						
-					}					
-				} else {
-					response.sendError(404);
-					response.getOutputStream().println("{ \"error\": \"Loading databases folder failed\"}");
-				}
-				result += "]";
-				result = result.replace(", ]", "]");
-				response.getOutputStream().println(result);
-			}
-		} catch (URISyntaxException e) {
+			String result = "[";
+		
+			for (XmlDataDatabase db: databasesContainer.getXmlDatabases()) {
+				if(db instanceof ServerXmlDataDatabase) {
+					ServerXmlDataDatabase serverDatabase = (ServerXmlDataDatabase) db;
+					result += "{\"LocalName\":";
+					result += "\"" + serverDatabase.getLocalName() + "\", ";
+					result += "\"Host\":";
+					result += "\"" + serverDatabase.getBaseXClient().getHost() + "\", ";
+					result += "\"Port\":";		
+					result += "\"" + Integer.toString(serverDatabase.getBaseXClient().getPort()) + "\", ";
+					result += "\"Name\":";		
+					result += "\"" + serverDatabase.getName() + "\", ";							
+					result += "}, ";
+				}						
+			}		
+			
+			result += "]";
+			result = result.replace(", ]", "]");
+			response.getOutputStream().println(result);
+		} else {
 			response.sendError(404);
 			response.getOutputStream().println("{ \"error\": \"Loading databases folder failed\"}");
 		}			
--- qualitypatternmodel/src/qualitypatternmodel/servlets/DatabasesRegistrationServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/DatabasesRegistrationServlet.java
@@ -46,77 +46,44 @@ public class DatabasesRegistrationServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		String requestUrl = request.getRequestURI();
-		String localName = requestUrl.substring(Util.DATABASE_REGISTRATION_ENDPOINT.length());	
-		
+		String localName = requestUrl.substring(Util.DATABASE_REGISTRATION_ENDPOINT.length());			
 		
 		String host = request.getParameter("host");
 		String portString = request.getParameter("port");
 		int port = Integer.parseInt(portString);		
 		String name = request.getParameter("name");
 		String user = request.getParameter("user");
-		String password = request.getParameter("password");
-
+		String password = request.getParameter("password");	
+				
+		String filePath = Util.DATABASES_PATH + Util.DATABASES_NAME + ".execution";	
+		URL folderURL = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
+		URL fileURL = getClass().getClassLoader().getResource(filePath);		
 		
-		try {
-			List<String> fileNames = Util.getListOfFileNamesInFolder(Util.DATABASES_PATH, getClass());
-			
-			if(fileNames == null) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Loading database folder failed\"}");
-			} else if(fileNames.isEmpty()) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"No databases found\"}");
-			} else if(fileNames.size() > 1) {
-				response.sendError(404);
-				response.getOutputStream().println("{ \"error\": \"Too many database containers found\"}");
-			} else {
-				String fileName = fileNames.get(0);
-				
-				String filePath = Util.DATABASES_PATH + fileName + ".execution";	
-				URL folderURL = getClass().getClassLoader().getResource(Util.DATABASES_PATH);
-				URL fileURL = getClass().getClassLoader().getResource(filePath);		
-				
-				if(fileURL != null && folderURL != null) {
-					DatabasesImpl.getInstance().clear();
-					Databases databasesContainer = EMFModelLoad.loadDatabases(fileURL.toString());
-					ServerXmlDataDatabase db = null;
-					try {
-						db = new ServerXmlDataDatabaseImpl(localName, host, port, name, user, password);
-						try {
-							db.createSchemaDatabaseFromReferencedSchema();
-							
-							EMFModelSave.exportToFile(databasesContainer, folderURL.toString() + fileName, "execution");
-							response.getOutputStream().println("Successfully registered database '" + localName + "'.");
-						} catch (InvalidityException e) {
-							response.sendError(404);
-							response.getOutputStream().println("{ \"error\": \"Analysing XML schema failed\"}");
-						}
-					} catch (InvalidityException e) {
-						response.sendError(409);
-						response.getOutputStream().println("{ \"error\": \"Local database name already used\"}");	
-					}
+		if(fileURL != null && folderURL != null) {
+			DatabasesImpl.getInstance().clear();
+			Databases databasesContainer = EMFModelLoad.loadDatabases(fileURL.toString());
+			ServerXmlDataDatabase db = null;
+			try {
+				db = new ServerXmlDataDatabaseImpl(localName, host, port, name, user, password);
+				try {
+					db.createSchemaDatabaseFromReferencedSchema();
 					
-					
-				} else {
-					response.sendError(404);
-					response.getOutputStream().println("{ \"error\": \"Loading databases folder failed\"}");
+					EMFModelSave.exportToFile(databasesContainer, folderURL.toString() + Util.DATABASES_NAME, "execution");
+					response.getOutputStream().println("Successfully registered database '" + localName + "'.");
+				} catch (InvalidityException | QueryException e) {
+					response.sendError(409);
+					response.getOutputStream().println("{ \"error\": \"Analysing XML schema failed\"}");
 				}
-			}			
+			} catch (InvalidityException e) {
+				response.sendError(409);
+				response.getOutputStream().println("{ \"error\": \"Local database name already used\"}");	
+			}
 			
 			
-		} catch (URISyntaxException e) {
+		} else {
 			response.sendError(404);
 			response.getOutputStream().println("{ \"error\": \"Loading databases folder failed\"}");
-		
-		} catch (QueryException e) {
-//			e.printStackTrace();
-			response.sendError(404);
-			response.getOutputStream().println("{ \"error\": \"Analysing XML schema failed\"}");
-		} 		
-		
-		
-
-		
+		}
 	
 	}	
 }
