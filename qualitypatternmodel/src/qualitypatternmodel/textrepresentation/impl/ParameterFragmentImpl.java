e703d5192eed1d83b549fae4f282157a8c2c9d93
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-10-27 11:41:41 +0200

    Correct type retrieval

--- qualitypatternmodel/src/qualitypatternmodel/textrepresentation/impl/ParameterFragmentImpl.java
+++ qualitypatternmodel/src/qualitypatternmodel/textrepresentation/impl/ParameterFragmentImpl.java
@@ -271,7 +271,7 @@ public class ParameterFragmentImpl extends FragmentImpl implements ParameterFrag
 	public String getType() {
 //		return getParameter().getClass().toString();
 		
-		Class type = getParameter().getClass();
+		Class type = getParameter().get(0).getClass();
 		if (type.equals(DateParamImpl.class)) {
 			return Constants.PARAMETER_TYPE_DATE;			
 		} else if(type.equals(TimeParamImpl.class)) {
@@ -300,7 +300,7 @@ public class ParameterFragmentImpl extends FragmentImpl implements ParameterFrag
 	 */
 	@Override
 	public String getRole() {
-		Class type = getParameter().getClass();
+		Class type = getParameter().get(0).getClass();
 		if (type.equals(DateParamImpl.class)) {
 			return Constants.PARAMETER_TYPE_DATE;			
 		} else if(type.equals(TimeParamImpl.class)) {
