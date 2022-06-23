e10fe92c8b561687026ec7bb69c011e6469de652
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-06-23 22:00:23 +0200

    Add base_test_pack & Further References

---
+++ qualitypatternmodel/test/qualitypatternmodel/translationtestsCypher/Test00.java
@@ -0,0 +1,13 @@
+package qualitypatternmodel.translationtestsCypher;
+
+import qualitypatternmodel.exceptions.InvalidityException;
+import qualitypatternmodel.exceptions.MissingPatternContainerException;
+import qualitypatternmodel.exceptions.OperatorCycleException;
+
+public class Test00 {
+	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		
+	}
+	
+	
+}
