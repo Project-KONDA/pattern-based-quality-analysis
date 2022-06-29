304261e293654f2c35eaa239ece1fb2dc8e850b2
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2022-06-29 12:09:46 +0200

    Add RDF test covering multiple return nodes

---
+++ qualitypatternmodel/test/qualitypatternmodel/rdftranslationtests/RdfTest02Return.java
@@ -0,0 +1,35 @@
+package qualitypatternmodel.rdftranslationtests;
+
+import java.util.ArrayList;
+
+import qualitypatternmodel.adaptionrdf.AdaptionrdfFactory;
+import qualitypatternmodel.adaptionrdf.IriParam;
+import qualitypatternmodel.adaptionrdf.RdfPredicate;
+import qualitypatternmodel.adaptionrdf.RdfQuantifier;
+import qualitypatternmodel.adaptionrdf.RdfSinglePredicate;
+import qualitypatternmodel.exceptions.InvalidityException;
+import qualitypatternmodel.exceptions.MissingPatternContainerException;
+import qualitypatternmodel.exceptions.OperatorCycleException;
+import qualitypatternmodel.patternstructure.CompletePattern;
+
+public class RdfTest02Return {
+	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
+		completePatterns.add(getMultipleReturnPattern());
+		RdfTest00.test(completePatterns);
+	}
+
+	private static CompletePattern getMultipleReturnPattern() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		CompletePattern completePattern = RdfTest01Predicates.getBasePatternPredicates();
+		completePattern.getGraph().getNodes().get(1).setReturnNode(true);
+		RdfPredicate relation = (RdfPredicate) completePattern.getGraph().getRelations().get(0);
+		IriParam iriParam = AdaptionrdfFactory.eINSTANCE.createIriParam();
+		RdfSinglePredicate rdfSinglePredicate = (RdfSinglePredicate) relation.getRdfPathParam();
+		rdfSinglePredicate.setIriParam(iriParam);
+		rdfSinglePredicate.setQuantifier(RdfQuantifier.ZERO_OR_ONE);
+		iriParam.setPrefix("wdt");
+		iriParam.setSuffix("P569");
+		
+		return completePattern;	
+	}
+}
