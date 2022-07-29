aa500670b352e4b17ff5e02e210c9eb70af795bd
kesperar <arno.kesper@uni-marburg.de>
2022-07-29 15:28:21 +0200

    EvalRefint.

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/EvalRefInt.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/EvalRefInt.java
@@ -19,14 +19,19 @@ import qualitypatternmodel.patternstructure.NotCondition;
 import qualitypatternmodel.patternstructure.PatternstructureFactory;
 import qualitypatternmodel.patternstructure.PatternstructurePackage;
 import qualitypatternmodel.patternstructure.QuantifiedCondition;
+import qualitypatternmodel.xmltranslationtests.Test00;
 
-public class EvalRefint {
+public class EvalRefInt {
 
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
+		
 		completePatterns.add(getRefintCondGeneric());
+		completePatterns.add(getRefintRunningExampleGeneric());
+		
 		for (CompletePattern cp: completePatterns)
-			System.out.println(cp.myToString());
+			Test00.printGenericPatternExampleXQuery(cp);
+//			System.out.println(cp.myToString());
 	}
 	
 	public static CompletePattern getRefintCondGeneric() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
@@ -76,6 +81,54 @@ public class EvalRefint {
 		
 		c.getOption().setValue(ComparisonOperator.EQUAL);
 		c.getTypeOption().setValue(ReturnType.STRING);
+		return completePattern; 
+	}
+
+	
+	public static CompletePattern getRefintRunningExampleGeneric() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		PatternstructurePackage.eINSTANCE.eClass();
+		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
+		GraphstructurePackage.eINSTANCE.eClass();
+		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
+				
+		CompletePattern completePattern = factory.createCompletePattern();
+		QuantifiedCondition qc1 = factory.createQuantifiedCondition(); 
+		NotCondition notc = factory.createNotCondition();
+		QuantifiedCondition qc2 = factory.createQuantifiedCondition();
+		
+		completePattern.setCondition(qc1);
+		qc1.setCondition(notc);
+		notc.setCondition(qc2);
+		
+		Graph g0 = completePattern.getGraph();
+		Graph g1 = qc1.getGraph();
+		Graph g2 = qc2.getGraph();
+		
+		Node e0g0 = g0.getReturnNodes().get(0);
+		Node e1g0 = e0g0.addOutgoing().getTarget().makePrimitive();
+		e1g0.addPrimitiveComparison();
+		
+		Node e0g1 = g1.getReturnNodes().get(0);
+		Node e2g1 = e0g1.addOutgoing().getTarget().makeComplex();
+		Node e3g1 = e2g1.addOutgoing().getTarget().makePrimitive();
+		e3g1.addPrimitiveComparison();
+		
+		Node e2g2 = g2.getNodes().get(2);
+		PrimitiveNode e4g2 = e2g2.addOutgoing().getTarget().makePrimitive();
+		
+		ComplexNode e5g2 = graphFactory.createComplexNode();
+		e5g2.setGraph(g2);
+		
+		PrimitiveNode e8g2 = e5g2.addOutgoing().getTarget().makePrimitive();
+		e8g2.addPrimitiveComparison();
+
+		PrimitiveNode e9g2 = e5g2.addOutgoing().getTarget().makePrimitive();
+		
+		Comparison c = e4g2.addComparison(e9g2);
+		
+		c.getOption().setValue(ComparisonOperator.EQUAL);
+		c.getTypeOption().setValue(ReturnType.STRING);
+		
 		return completePattern; 
 	}
 }
--- qualitypatternmodel/test/qualitypatternmodel/xmlevaluation/XmlEvalRefInt.java
+++ qualitypatternmodel/test/qualitypatternmodel/xmlevaluation/XmlEvalRefInt.java
@@ -16,7 +16,7 @@ import qualitypatternmodel.parameters.UntypedParameterValue;
 import qualitypatternmodel.adaptionxml.XmlPropertyKind;
 import qualitypatternmodel.adaptionxml.XmlAxisKind;
 import qualitypatternmodel.adaptionxml.XmlPathParam;
-import qualitypatternmodel.evaluation.EvalRefint;
+import qualitypatternmodel.evaluation.EvalRefInt;
 import qualitypatternmodel.exceptions.InvalidityException;
 import qualitypatternmodel.exceptions.MissingPatternContainerException;
 import qualitypatternmodel.exceptions.OperatorCycleException;
@@ -28,12 +28,12 @@ import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.QuantifiedCondition;
 import qualitypatternmodel.xmltranslationtests.Test00;
 
-public class XmlEvalRefint {
+public class XmlEvalRefInt {
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
 		completePatterns.add(getRefintCondAbstract());
 		completePatterns.add(getRefintMidasWer());
-		completePatterns.add(getRefintAbstractRunningExample());
+		completePatterns.add(getRefintRunningExampleAbstract());
 		completePatterns.add(getRefintRunningExample());
 		
 		Test00.getQueries(completePatterns);
@@ -41,8 +41,14 @@ public class XmlEvalRefint {
 	}
 	
 	public static CompletePattern getRefintCondAbstract() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		CompletePattern completePattern = EvalRefint.getRefintCondGeneric();
+		CompletePattern completePattern = EvalRefInt.getRefintCondGeneric();
 		completePattern.createXmlAdaption();		
+		return completePattern; 
+	}
+	
+	public static CompletePattern getRefintRunningExampleAbstract() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		CompletePattern completePattern = EvalRefInt.getRefintRunningExampleGeneric();
+		completePattern.createXmlAdaption();
 		return completePattern; 
 	}
 	
@@ -101,57 +107,8 @@ public class XmlEvalRefint {
 		return completePattern;		
 	}
 	
-	public static CompletePattern getRefintAbstractRunningExample() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		PatternstructurePackage.eINSTANCE.eClass();
-		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
-		GraphstructurePackage.eINSTANCE.eClass();
-		GraphstructureFactory graphFactory = GraphstructureFactory.eINSTANCE;
-				
-		CompletePattern completePattern = factory.createCompletePattern();
-		QuantifiedCondition qc1 = factory.createQuantifiedCondition(); 
-		NotCondition notc = factory.createNotCondition();
-		QuantifiedCondition qc2 = factory.createQuantifiedCondition();
-		
-		completePattern.setCondition(qc1);
-		qc1.setCondition(notc);
-		notc.setCondition(qc2);
-		
-		Graph g0 = completePattern.getGraph();
-		Graph g1 = qc1.getGraph();
-		Graph g2 = qc2.getGraph();
-		
-		Node e0g0 = g0.getReturnNodes().get(0);
-		Node e1g0 = e0g0.addOutgoing().getTarget().makePrimitive();
-		e1g0.addPrimitiveComparison();
-		
-		Node e0g1 = g1.getReturnNodes().get(0);
-		Node e2g1 = e0g1.addOutgoing().getTarget().makeComplex();
-		Node e3g1 = e2g1.addOutgoing().getTarget().makePrimitive();
-		e3g1.addPrimitiveComparison();
-		
-		Node e2g2 = g2.getNodes().get(2);
-		PrimitiveNode e4g2 = e2g2.addOutgoing().getTarget().makePrimitive();
-		
-		ComplexNode e5g2 = graphFactory.createComplexNode();
-		e5g2.setGraph(g2);
-		
-		PrimitiveNode e8g2 = e5g2.addOutgoing().getTarget().makePrimitive();
-		e8g2.addPrimitiveComparison();
-
-		PrimitiveNode e9g2 = e5g2.addOutgoing().getTarget().makePrimitive();
-		
-		Comparison c = e4g2.addComparison(e9g2);
-		
-		c.getOption().setValue(ComparisonOperator.EQUAL);
-		c.getTypeOption().setValue(ReturnType.STRING);
-		
-		completePattern.createXmlAdaption();
-		
-		return completePattern; 
-	}
-	
 	public static CompletePattern getRefintRunningExample() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		CompletePattern completePattern = getRefintAbstractRunningExample();
+		CompletePattern completePattern = getRefintRunningExampleAbstract();
 		List<Parameter> params = completePattern.getParameterList().getParameters();
 		
 		UntypedParameterValue p0 = ((UntypedParameterValue) params.get(0));
