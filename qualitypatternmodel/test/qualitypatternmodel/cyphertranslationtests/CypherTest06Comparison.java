41166dd9839990b820ec6ddab09b01911f7e7e57
Lukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-09-10 22:25:50 +0200

    Almost all tests are implemented. Remaining are: CountTester, and ContainsTester as well as optimazation of the other tests

--- qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/CypherTest06Comparison.java
+++ qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/CypherTest06Comparison.java
@@ -33,8 +33,8 @@ public class CypherTest06Comparison {
             System.out.println("");
             System.out.println("<<< BEGIN - Tests >>>");
             ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
-//            completePatterns.add(getComparisonTwoNeoNodes(ComparisonOperator.EQUAL));
-//            completePatterns.add(getComparisonTwoNeoNodes(ComparisonOperator.NOTEQUAL));
+            completePatterns.add(getComparisonTwoNeoNodes(ComparisonOperator.EQUAL));
+            completePatterns.add(getComparisonTwoNeoNodes(ComparisonOperator.NOTEQUAL));
 //            for (ComparisonOperator c : ComparisonOperator.values()) { // --> Could be done later one when all operators are implemented
 //            	if (!(c == ComparisonOperator.ISNULL || c == ComparisonOperator.ISNOTNULL)) {
 //            		completePatterns.add(getComparisonTwoNeoNodes(c));
