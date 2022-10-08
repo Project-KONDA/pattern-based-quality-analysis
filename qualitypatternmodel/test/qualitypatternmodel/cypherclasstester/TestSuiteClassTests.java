2097afe9752c02a34c9b1c2cfb6ca463af317282
Lukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-10-08 16:23:39 +0200

    Added the jupiter-engine & platform-suite-enginge to the project. Missing is the adding to the BuildPath

--- qualitypatternmodel/test/qualitypatternmodel/cypherclasstester/TestSuiteClassTests.java
+++ qualitypatternmodel/test/qualitypatternmodel/cypherclasstester/TestSuiteClassTests.java
@@ -3,6 +3,7 @@ package qualitypatternmodel.cypherclasstester;
 //import org.junit.platform.suite.api.SelectClasses;
 //import org.junit.platform.suite.api.SelectPackages;
 //import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.Suite;
 
 import qualitypatternmodel.cypherclasstester.concretetests.Neo01NodeTest;
 
