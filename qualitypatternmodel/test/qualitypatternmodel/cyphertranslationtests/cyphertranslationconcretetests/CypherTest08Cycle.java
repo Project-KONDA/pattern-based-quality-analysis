d66ef8347a8613f0f264dcfdf66fa4de21f092a5
Lukas <Lukas-Sebastian.Hofmann@gmx.de>
2022-10-29 17:37:41 +0200

    Some further testing

--- qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/cyphertranslationconcretetests/CypherTest08Cycle.java
+++ qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/cyphertranslationconcretetests/CypherTest08Cycle.java
@@ -1,7 +1,5 @@
 package qualitypatternmodel.cyphertranslationtests.cyphertranslationconcretetests;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.ArrayList;
 
 import qualitypatternmodel.cyphertranslationtests.CypherAbstractTranslation;
@@ -15,27 +13,24 @@ import qualitypatternmodel.patternstructure.CompletePattern;
 public class CypherTest08Cycle extends CypherAbstractTranslation {
     public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
     	CypherTest08Cycle cycle = new CypherTest08Cycle();
-    	Instant start = Instant.now();
+//    	Instant start = Instant.now();
     	try {
-//    		cycle.generalizedTests();         
-//    		cycle.generalizedInvalidtyExceptionTests();
-    		ArrayList<CompletePattern> l = new ArrayList<>();
-    		cycle.buildPatterns(l);
-    		System.out.println(l.get(0).generateCypher());
-    		System.out.println(l.get(1).generateCypher());    		
+    		cycle.generalizedTests();         
+    		cycle.generalizedInvalidtyExceptionTests();
     	} catch (Exception e) {
 			System.out.println(e);
 		}
-		    
-		Instant finish = Instant.now();
-		long timeElapsed = Duration.between(start, finish).toMillis();
-		System.out.println(timeElapsed);
+		
+		// CODE HERE        
+//		Instant finish = Instant.now();
+//		long timeElapsed = Duration.between(start, finish).toMillis();
+//		System.out.println(timeElapsed);
     }
 
 	public void buildPatterns(ArrayList<CompletePattern> completePatterns)
 			throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		completePatterns.add(getCyclePattern2());
-        completePatterns.add(getCyclePattern3());
+		completePatterns.add(getCyclePattern(2));
+        completePatterns.add(getCyclePattern(3));
 	}
 	
 	@Override
@@ -44,32 +39,20 @@ public class CypherTest08Cycle extends CypherAbstractTranslation {
 		//Nothing to generate here		
 	}
     
-	private CompletePattern getCyclePattern2() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+	private CompletePattern getCyclePattern(int nodesInCycle) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		CompletePattern completePattern = CypherAbstractTranslation.getBasePattern();
 		ComplexNode newNode0 = (ComplexNode) completePattern.getGraph().getNodes().get(0);
-		ComplexNode newNode1 = (ComplexNode) completePattern.getGraph().addComplexNode();
-		completePattern.getGraph().addRelation(newNode0, newNode1);
-		ComplexNode newNode2 = (ComplexNode) completePattern.getGraph().addComplexNode();
-		completePattern.getGraph().addRelation(newNode0, newNode2);
+		ComplexNode[] newNode = new ComplexNode[nodesInCycle];
+		for (int i = 0; i < nodesInCycle; i++) {
+			newNode[i] = completePattern.getGraph().addComplexNode();
+			if (i == 0) {
+				completePattern.getGraph().addRelation(newNode0, newNode[i]);
+			} else {
+				completePattern.getGraph().addRelation(newNode[i - 1], newNode[i]);
+			}
+		}
 		
-		newNode0.addComparison(newNode2);
-		
-		completePattern.createNeo4jAdaption();
-		
-		return completePattern;
-	}
-	
-	private CompletePattern getCyclePattern3() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		CompletePattern completePattern = CypherAbstractTranslation.getBasePattern();
-		ComplexNode newNode0 = (ComplexNode) completePattern.getGraph().getNodes().get(0);
-		ComplexNode newNode1 = (ComplexNode) completePattern.getGraph().addComplexNode();
-		completePattern.getGraph().addRelation(newNode0, newNode1);
-		ComplexNode newNode2 = (ComplexNode) completePattern.getGraph().addComplexNode();
-		completePattern.getGraph().addRelation(newNode0, newNode2);
-		ComplexNode newNode3 = (ComplexNode) completePattern.getGraph().addComplexNode();
-		completePattern.getGraph().addRelation(newNode0, newNode3);
-		
-		newNode0.addComparison(newNode3);
+		newNode0.addComparison(newNode[nodesInCycle - 1]);
 		
 		completePattern.createNeo4jAdaption();
 		
