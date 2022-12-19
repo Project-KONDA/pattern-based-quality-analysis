3d3d82da34403f15a6a7d9081e9bb16d5ced1fa5
kesperar <arno.kesper@uni-marburg.de>
2022-12-19 16:16:07 +0100

    Fix Quantified Condition with Forall in RDF by not translating Forall-Clausels without inner condition.

--- qualitypatternmodel/test/qualitypatternmodel/rdftranslationtests/RdfTest03Quantor.java
+++ qualitypatternmodel/test/qualitypatternmodel/rdftranslationtests/RdfTest03Quantor.java
@@ -1,5 +1,8 @@
 package qualitypatternmodel.rdftranslationtests;
 
+//import static org.junit.Assert.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.ArrayList;
 
 import qualitypatternmodel.exceptions.InvalidityException;
@@ -16,15 +19,37 @@ public class RdfTest03Quantor {
 	
 	public static ArrayList<CompletePattern> getPatterns() throws InvalidityException, OperatorCycleException, MissingPatternContainerException{
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
+//		assertThrows(InvalidityException.class, () -> getQuantifierQuantifierPattern(Quantifier.FORALL).generateSparql());
+//		assertThrows(InvalidityException.class, () -> getQuantifierQuantifierPattern(Quantifier.EXISTS, Quantifier.FORALL).generateSparql());
+//		assertThrows(InvalidityException.class, () -> getQuantifierQuantifierPattern(Quantifier.FORALL, Quantifier.FORALL).generateSparql());
+		
+		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.EXISTS));
 		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.EXISTS, Quantifier.EXISTS));
-		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.EXISTS, Quantifier.FORALL));
 		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.FORALL, Quantifier.EXISTS));
+
+		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.FORALL));
+		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.EXISTS, Quantifier.FORALL));
 		completePatterns.add(getQuantifierQuantifierPattern(Quantifier.FORALL, Quantifier.FORALL));
+		
 		return completePatterns;
 	}
 
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		RdfTest00.test(getPatterns());
+	}
+
+	private static CompletePattern getQuantifierQuantifierPattern(Quantifier q) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		CompletePattern completePattern = RdfTest00.getBasePattern();
+		QuantifiedCondition quantifiedCond = PatternstructureFactory.eINSTANCE.createQuantifiedCondition();
+		completePattern.setCondition(quantifiedCond);
+		quantifiedCond.setQuantifier(q);
+		
+		ComplexNode complexNode1 = quantifiedCond.getGraph().getNodes().get(1).makeComplex();
+		Node node2 = quantifiedCond.getGraph().addNode();
+		quantifiedCond.getGraph().addRelation(complexNode1, node2);
+		
+		completePattern.createRdfAdaption();
+		return completePattern;
 	}
 
 	private static CompletePattern getQuantifierQuantifierPattern(Quantifier q, Quantifier q2) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
