79fdb30cbdeaf8ab2716d50a5d9346a6fe13174d
kesperar <arno.kesper@uni-marburg.de>
2023-01-09 15:20:39 +0100

    Revert "Simplify some Eval Patterns."
    This reverts commit 7100a0227421b9f1f1910a4cf2a5ec70caa7270b.

--- qualitypatternmodel/test/qualitypatternmodel/evaluation/EvalMatch.java
+++ qualitypatternmodel/test/qualitypatternmodel/evaluation/EvalMatch.java
@@ -18,11 +18,9 @@ public class EvalMatch {
 
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		ArrayList<CompletePattern> completePatterns = new ArrayList<CompletePattern>();
-
+		
 		completePatterns.add(getMatchGeneric());
 		completePatterns.add(getMatch3Generic());
-		completePatterns.add(getMatchGenericLong());
-		completePatterns.add(getMatch3GenericLong());
 		
 		for (CompletePattern cp: completePatterns)
 			Test00.printGenericPatternExampleXQuery(cp);
@@ -30,16 +28,6 @@ public class EvalMatch {
 	}
 	
 	public static CompletePattern getMatchGeneric() throws InvalidityException {
-		CompletePattern completePattern = PatternstructureFactory.eINSTANCE.createCompletePattern();
-		
-		Node element0 = completePattern.getGraph().getReturnNodes().get(0).makeComplex();
-		Node element1 = element0.addOutgoing().getTarget().makePrimitive();
-		element1.addPrimitiveMatch();
-		
-		return completePattern;	
-	}
-	
-	public static CompletePattern getMatchGenericLong() throws InvalidityException {
 		CompletePattern completePattern = PatternstructureFactory.eINSTANCE.createCompletePattern();
 		
 		completePattern.getGraph().getReturnNodes().get(0).makeComplex();
@@ -77,22 +65,6 @@ public class EvalMatch {
 //	}
 	
 	public static CompletePattern getMatch3Generic() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
-		PatternstructurePackage.eINSTANCE.eClass();
-		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
-		
-		CompletePattern completePattern = factory.createCompletePattern();
-		
-		Graph g = completePattern.getGraph();
-		
-		Node return1 = g.getReturnNodes().get(0).makeComplex();
-		Node node1 = return1.addOutgoing().getTarget().makeComplex();
-		Node node2 = node1.addOutgoing().getTarget().makeComplex();
-		node2.addOutgoing().getTarget().addPrimitiveMatch();
-
-		return completePattern;
-	}
-	
-	public static CompletePattern getMatch3GenericLong() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		PatternstructurePackage.eINSTANCE.eClass();
 		PatternstructureFactory factory = PatternstructureFactory.eINSTANCE;
 		
