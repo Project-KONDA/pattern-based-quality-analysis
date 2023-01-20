ca143f124358caac0c1b9593375a3a518110c365
Lukas <Lukas-Sebastian.Hofmann@gmx.de>
2023-01-20 15:41:09 +0100

    Test imps

--- qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/cyphertranslationconcretetests/CypherTest01NeoPropertyEdge.java
+++ qualitypatternmodel/test/qualitypatternmodel/cyphertranslationtests/cyphertranslationconcretetests/CypherTest01NeoPropertyEdge.java
@@ -27,7 +27,7 @@ public class CypherTest01NeoPropertyEdge extends CypherTranslation {
 	public static void main(String[] args) throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		CypherTest01NeoPropertyEdge neoPropertyEdge = new CypherTest01NeoPropertyEdge();		
 		try {
-//			neoPropertyEdge.generalizedTests(); 
+			neoPropertyEdge.generalizedTests(); 
 			neoPropertyEdge.generalizedComplexTests();
 //			neoPropertyEdge.generalizedInvalidtyExceptionTests();
 		} catch (Exception e) {
