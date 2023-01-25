92ac8ae40dcb1449b489912e8d558734180e25be
kesperar <arno.kesper@uni-marburg.de>
2023-01-25 15:49:02 +0100

    Update servelet endpoint konstants.

--- qualitypatternmodel/src/qualitypatternmodel/servlets/Util.java
+++ qualitypatternmodel/src/qualitypatternmodel/servlets/Util.java
@@ -17,29 +17,29 @@ public class Util {
 	public static final String PROJECT_PREFIX_ENDPOINT = "/qualitypatternmodel";
 	
 	public static final String PARAMETER_EXPLANATIONS_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/parameter-explanations";
-	public static final String ABSTRACT_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/abstract-patterns";
-	public static final String CONCRETE_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns";
-	public static final String DATABASES_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/databases";
+	public static final String ABSTRACT_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/abstract-patterns";
+	public static final String CONCRETE_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns";
+	public static final String DATABASES_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/databases";
 
-	public static final String ABSTRACT_PATTERN_TEXT_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/abstract-patterns/text-list/";
-	public static final String INSTANTIATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/abstract-patterns/instantiation/";
-	public static final String CONCRETE_PATTERN_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/deletion/";
-	public static final String CONCRETE_PATTERN_TEXT_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/text/";
-	public static final String ABSTRACT_PATTERN_TEXT_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/abstract-patterns/text/deletion/";
-	public static final String CONCRETISATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/parameter/";
-	public static final String APPLICATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/finalized-patterns/application";
-	public static final String DESCRIPTION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/description/";
-	public static final String CONCRETISATION_FINALIZATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/finalization/";
-	public static final String SCHEMA_VALIDATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/schema-validation/";
-	public static final String QUERY_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/concrete-patterns/query/";
-	public static final String FINALIZED_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/finalized-patterns";
-	public static final String FINALIZED_PATTERN_DATABASE_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/finalized-patterns/database/";
+	public static final String ABSTRACT_PATTERN_TEXT_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/abstract-patterns/text-list/";
+	public static final String INSTANTIATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/abstract-patterns/instantiation/";
+	public static final String CONCRETE_PATTERN_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/deletion/";
+	public static final String CONCRETE_PATTERN_TEXT_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/text/";
+	public static final String ABSTRACT_PATTERN_TEXT_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/abstract-patterns/text/deletion/";
+	public static final String CONCRETISATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/parameter/";
+	public static final String APPLICATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/finalized-patterns/application";
+	public static final String DESCRIPTION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/description/";
+	public static final String CONCRETISATION_FINALIZATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/finalization/";
+	public static final String SCHEMA_VALIDATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/schema-validation/";
+	public static final String QUERY_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/concrete-patterns/query/";
+	public static final String FINALIZED_PATTERN_LIST_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/finalized-patterns";
+	public static final String FINALIZED_PATTERN_DATABASE_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/finalized-patterns/database/";
 
 	
-	public static final String DATABASE_REGISTRATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/databases/registration/";
-	public static final String DATABASE_SETTING_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/databases/set/";
-	public static final String DATABASE_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/databases/deletion/";
-	public static final String DATABASE_GETTING_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/databases/get/";
+	public static final String DATABASE_REGISTRATION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/databases/registration/";
+	public static final String DATABASE_SETTING_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/databases/set/";
+	public static final String DATABASE_DELETION_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/databases/deletion/";
+	public static final String DATABASE_GETTING_ENDPOINT = PROJECT_PREFIX_ENDPOINT + "/xml/databases/get/";
 
 
 
