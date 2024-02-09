7070897915078f446ffb678e9ebb07bbd1781a72
Arno <arno.kesper@uni-marburg.de>
2024-02-09 13:47:50 +0100

    Partially implement TemplateMqafJsonServlet. Requires merge with constraint_translation_experiment branch.

--- qualitypatternmodel/src/qualitypatternmodel/newservlets/TemplateMqafJsonServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/newservlets/TemplateMqafJsonServlet.java
@@ -1,8 +1,81 @@
 package qualitypatternmodel.newservlets;
 
+import java.io.IOException;
+import java.util.Map;
+
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import qualitypatternmodel.exceptions.FailedServletCallException;
+import qualitypatternmodel.exceptions.InvalidServletCallException;
+import qualitypatternmodel.patternstructure.AbstractionLevel;
+import qualitypatternmodel.patternstructure.CompletePattern;
 
 @SuppressWarnings("serial")
 public class TemplateMqafJsonServlet extends HttpServlet {
+	
+	// .. /template/mqafjson   /<technology>/<constraintId>
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String path = request.getPathInfo();
+		System.out.println("TemplateMqafJsonServlet.doGet(" + path + ")");
+		try {
+//			String result = applyGet(path, request.getParameterMap());
+//			response.getOutputStream().println(result);
+//			response.setStatus(HttpServletResponse.SC_OK);
+			response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+			response.getWriter().write("{ \"error\": \"not implemented\"}");
+		}
+//		catch (InvalidServletCallException e) {
+//	        response.setContentType("application/json");
+//			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+//		}
+//		catch (FailedServletCallException e) {
+//	        response.setContentType("application/json");
+//			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+//		}
+		catch (Exception e) {
+	        response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+		}
+//		response.getOutputStream().println("{ \"call\": \"TemplateMqafJsonServlet.doGet(" + path + ")\"}");
+	}
 
+	public String applyGet(String path, Map<String, String[]> parameterMap) throws InvalidServletCallException, FailedServletCallException {
+		String[] pathparts = path.split("/");
+		if (pathparts.length != 3 || !pathparts[0].equals(""))
+			throw new InvalidServletCallException("Wrong url for requesting the datamodel of a constraint: '.. /template/getdatamodel/<technology>/<name>' (not " + path + ")");
+
+		String technology = pathparts[1];
+		String patternname = pathparts[2];
+		String patternpath = "serverpatterns/" + technology + "/concrete-patterns/" + patternname + ".pattern";
+
+		// 1 load constraint
+		CompletePattern pattern;
+		try {
+			pattern = ServletUtilities.loadCompletePattern(patternpath);
+		} catch (Exception e) {
+			throw new FailedServletCallException("constraint not found: " + e.getMessage());
+		}
+		if (pattern == null) {
+			throw new FailedServletCallException("constraint not found");
+		}
+		
+		try {
+			pattern.isValid(AbstractionLevel.CONCRETE);
+		} catch (Exception e) {
+			throw new FailedServletCallException(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		// 2 generate mqaf constraint
+//		String constraint;
+		
+		// return constraint
+//		return constraint;
+		throw new FailedServletCallException("not implemented");
+	}
 }
