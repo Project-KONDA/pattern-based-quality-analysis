21496252be453258ea4c8932dcb06f8203cb7142
Arno <arno.kesper@uni-marburg.de>
2024-02-09 13:47:08 +0100

    Implement TemplateQueryServlet.

--- qualitypatternmodel/src/qualitypatternmodel/newservlets/TemplateQueryServlet.java
+++ qualitypatternmodel/src/qualitypatternmodel/newservlets/TemplateQueryServlet.java
@@ -1,8 +1,104 @@
 package qualitypatternmodel.newservlets;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import qualitypatternmodel.exceptions.FailedServletCallException;
+import qualitypatternmodel.exceptions.InvalidServletCallException;
+import qualitypatternmodel.exceptions.InvalidityException;
+import qualitypatternmodel.patternstructure.AbstractionLevel;
+import qualitypatternmodel.patternstructure.CompletePattern;
 
 @SuppressWarnings("serial")
-public class TemplateQueryServlet extends HttpServlet{
+public class TemplateQueryServlet extends HttpServlet {
+	
+	// .. /template/query   /<technology>/<constraintId>
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String path = request.getPathInfo();
+		Map<String, String[]> params = request.getParameterMap();
+		System.out.println("TemplateQueryServlet.doGet(" + path + ")");
+		try {
+			String result = applyGet(path, params);
+			response.getOutputStream().println(result);
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
+		catch (InvalidServletCallException e) {
+	        response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+		}
+		catch (FailedServletCallException e) {
+	        response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+		}
+		catch (Exception e) {
+	        response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write("{ \"error\": \"" + e.getMessage() + "\"}");
+		}
+//		response.getOutputStream().println("{ \"call\": \"TemplateQueryServlet.doGet(" + path + ")\"}");
+	}
 
+	public String applyGet(String path, Map<String, String[]> parameterMap) throws InvalidServletCallException, FailedServletCallException {
+		String[] pathparts = path.split("/");
+		if (pathparts.length != 3 || !pathparts[0].equals(""))
+			throw new InvalidServletCallException("Wrong url for requesting the datamodel of a constraint: '.. /template/getdatamodel/<technology>/<name>' (not " + path + ")");
+
+		String technology = pathparts[1];
+		String patternname = pathparts[2];
+		String patternpath = "serverpatterns/" + technology + "/concrete-patterns/" + patternname + ".pattern";
+
+		// 1 load constraint
+		CompletePattern pattern;
+		try {
+			pattern = ServletUtilities.loadCompletePattern(patternpath);
+		} catch (Exception e) {
+			throw new FailedServletCallException("constraint not found: " + e.getMessage());
+		}
+		if (pattern == null) {
+			throw new FailedServletCallException("constraint not found");
+		}
+		
+		try {
+			pattern.isValid(AbstractionLevel.CONCRETE);
+		} catch (Exception e) {
+			throw new FailedServletCallException(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		// 2 generate query
+		String query;
+		try {
+			switch(technology) {
+			case "xml":
+				query = pattern.generateXQuery();
+				break;
+			case "sql":
+				query = pattern.generateSparql();
+				break;
+			case "neo4j":
+				query = pattern.generateCypher();
+				break;
+			default:
+				throw new InvalidServletCallException();
+			}
+		} catch (InvalidityException e) {
+			throw new FailedServletCallException();
+		}
+		
+		// 3 return query
+		try {
+			return new JSONObject().put("query", query ).toString();
+		} catch (JSONException e) {
+			return "{\"query\":\"" + query + "\"}"; 
+		}
+	}
 }
