3988cfe488f1b575974bce80682595ebc11d1358
Arno <arno.kesper@uni-marburg.de>
2025-08-05 17:14:31 +0200

    Adapt other classes to changes.

--- qualitypatternmodel/src/qualitypatternmodel/newservlets/initialisation/PatternCollection.java
+++ qualitypatternmodel/src/qualitypatternmodel/newservlets/initialisation/PatternCollection.java
@@ -7,7 +7,27 @@ import org.eclipse.emf.common.util.BasicEList;
 import qualitypatternmodel.exceptions.InvalidityException;
 import qualitypatternmodel.exceptions.MissingPatternContainerException;
 import qualitypatternmodel.exceptions.OperatorCycleException;
+import qualitypatternmodel.newservlets.patterns.Appdup2Pattern;
+import qualitypatternmodel.newservlets.patterns.Appdup3Pattern;
+import qualitypatternmodel.newservlets.patterns.CardImpliesMandAttPattern;
+import qualitypatternmodel.newservlets.patterns.CardPattern;
+import qualitypatternmodel.newservlets.patterns.CheckFormatPattern;
+import qualitypatternmodel.newservlets.patterns.CompDatabasePattern;
+import qualitypatternmodel.newservlets.patterns.CompPattern;
+import qualitypatternmodel.newservlets.patterns.CompSetPattern;
+import qualitypatternmodel.newservlets.patterns.CompValAnyPattern;
+import qualitypatternmodel.newservlets.patterns.CompValPattern;
+import qualitypatternmodel.newservlets.patterns.ContainsPattern;
+import qualitypatternmodel.newservlets.patterns.DupvalPattern;
+import qualitypatternmodel.newservlets.patterns.FuncPattern;
+import qualitypatternmodel.newservlets.patterns.LocalUniquePattern;
+import qualitypatternmodel.newservlets.patterns.MandAttPattern;
+import qualitypatternmodel.newservlets.patterns.MandContPattern;
+import qualitypatternmodel.newservlets.patterns.MatchPattern;
 import qualitypatternmodel.newservlets.patterns.PatternClass;
+import qualitypatternmodel.newservlets.patterns.StringLengthPattern;
+import qualitypatternmodel.newservlets.patterns.UniquePattern;
+import qualitypatternmodel.newservlets.patterns.ValidLinkPattern;
 import qualitypatternmodel.patternstructure.CompletePattern;
 
 public class PatternCollection {
@@ -15,6 +35,26 @@ public class PatternCollection {
 
 	public static List<Class<? extends PatternClass>> getPatternClasses() {
 		List<Class<? extends PatternClass>> classes = new BasicEList<Class<? extends PatternClass>>();
+		classes.add(Appdup2Pattern.class);
+		classes.add(Appdup3Pattern.class);
+		classes.add(CardImpliesMandAttPattern.class);
+		classes.add(CardPattern.class);
+		classes.add(CheckFormatPattern.class);
+		classes.add(CompDatabasePattern.class);
+		classes.add(CompPattern.class);
+		classes.add(CompSetPattern.class);
+		classes.add(CompValAnyPattern.class);
+		classes.add(CompValPattern.class);
+		classes.add(ContainsPattern.class);
+		classes.add(DupvalPattern.class);
+		classes.add(FuncPattern.class);
+		classes.add(LocalUniquePattern.class);
+		classes.add(MandAttPattern.class);
+		classes.add(MandContPattern.class);
+		classes.add(MatchPattern.class);
+		classes.add(StringLengthPattern.class);
+		classes.add(UniquePattern.class);
+		classes.add(ValidLinkPattern.class);
 		return classes;
 	}
 
