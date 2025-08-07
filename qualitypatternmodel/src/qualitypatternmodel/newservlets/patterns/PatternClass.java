8d78c3b9e93d7f75d5f984b74e3785ad463d39fd
Arno <arno.kesper@uni-marburg.de>
2025-08-07 13:11:54 +0200

    Further improve PatternClass and PatternCollection.

--- qualitypatternmodel/src/qualitypatternmodel/newservlets/patterns/PatternClass.java
+++ qualitypatternmodel/src/qualitypatternmodel/newservlets/patterns/PatternClass.java
@@ -17,6 +17,10 @@ abstract public class PatternClass {
 
 	// Add information
 	public final String id;
+	public final String genericId;
+	public final String xmlId;
+	public final String rdfId;
+	public final String neoId;
 	public final String name;
 	public final String description;
 	public final Boolean genericValid;
@@ -26,6 +30,10 @@ abstract public class PatternClass {
 
 	protected PatternClass(String id, String name, String description, Boolean genericValid, Boolean xmlValid, Boolean rdfValid, Boolean neoValid) {
 		this.id = id;
+		genericId = id + "_generic";
+		xmlId = id + "_xml";
+		rdfId = id + "_rdf";
+		neoId = id + "_neo4j";
 		this.name = name;
 		this.description = description;
 		this.genericValid = genericValid;
@@ -33,23 +41,6 @@ abstract public class PatternClass {
 		this.rdfValid = rdfValid;
 		this.neoValid = neoValid;
 	}
-
-	String genericId() {
-		return id + "_generic";
-	};
-
-	String xmlId() {
-		return id + "_xml";
-	};
-
-	String rdfId() {
-		return id + "_rdf";
-	};
-
-	String neoId() {
-		return id + "_neo4j";
-	};
-
 
 	public CompletePattern getGeneric() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		CompletePattern pattern = getPattern();
@@ -63,17 +54,19 @@ abstract public class PatternClass {
 	}
 	
 	protected void setInfo(CompletePattern pattern) {
-		pattern.setPatternId(genericId());
-		pattern.setAbstractId(genericId());
+		pattern.setPatternId(genericId);
+		pattern.setAbstractId(genericId);
 		pattern.setName(name);
 		pattern.setDescription(description);
 	}
+
+	// PatternBundes
 
 	public PatternBundle getXmlBundle() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
 		return new PatternBundle(
 				getGeneric(),
 				Language.XML,
-				xmlId(),
+				xmlId,
 				xmlMap(),
 				xmlVariants(),
 				xmlVariantsOld());
@@ -83,7 +76,7 @@ abstract public class PatternClass {
 		return new PatternBundle(
 				getGeneric(),
 				Language.RDF,
-				rdfId(),
+				rdfId,
 				rdfMap(),
 				rdfVariants(),
 				rdfVariantsOld());
@@ -93,10 +86,22 @@ abstract public class PatternClass {
 		return new PatternBundle(
 				getGeneric(),
 				Language.NEO4J,
-				neoId(),
+				neoId,
 				neoMap(),
 				neoVariants(),
 				neoVariantsOld());
+	}
+	
+	public CompletePattern getXmlPattern() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		return getXmlBundle().getConcrete();
+	}
+	
+	public CompletePattern getRdfPattern() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		return getRdfBundle().getConcrete();
+	}
+	
+	public CompletePattern getNeoPattern() throws InvalidityException, OperatorCycleException, MissingPatternContainerException {
+		return getNeoBundle().getConcrete();
 	}
 
 	// Override for specific Patternbundle options
