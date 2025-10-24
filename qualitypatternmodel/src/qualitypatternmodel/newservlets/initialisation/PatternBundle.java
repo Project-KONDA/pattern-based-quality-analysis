40feebb358920296a8db61eeebc372cdfc097150
Arno <arno.kesper@uni-marburg.de>
2025-10-24 15:15:32 +0200

    Fix override problem.

--- qualitypatternmodel/src/qualitypatternmodel/newservlets/initialisation/PatternBundle.java
+++ qualitypatternmodel/src/qualitypatternmodel/newservlets/initialisation/PatternBundle.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import qualitypatternmodel.exceptions.InvalidityException;
 import qualitypatternmodel.exceptions.MissingPatternContainerException;
 import qualitypatternmodel.exceptions.OperatorCycleException;
@@ -16,8 +14,6 @@ import qualitypatternmodel.parameters.Parameter;
 import qualitypatternmodel.patternstructure.AbstractionLevel;
 import qualitypatternmodel.patternstructure.CompletePattern;
 import qualitypatternmodel.patternstructure.Language;
-import qualitypatternmodel.textrepresentation.impl.PatternTextImpl;
-import qualitypatternmodel.utility.ConstantsJSON;
 
 public class PatternBundle {
 	CompletePattern genericpattern;
@@ -41,21 +37,22 @@ public class PatternBundle {
 	}
 
 	public void exportTemplate() throws JSONException, InvalidityException, IOException, OperatorCycleException, MissingPatternContainerException {
-		if (ServletConstants.OVERRIDE_VARIANTS) {
+		if (!ServletConstants.OVERRIDE_VARIANTS) {
 			try {
-				CompletePattern existing = ServletUtilities.loadTemplate(language.getLiteral(), id);
-				for (String variant: variants) {
-					JSONObject var = new JSONObject(variant);
-					Boolean typeConstraint = var.getBoolean(ConstantsJSON.TYPE_CONSTRAINT);
-					if (typeConstraint && ServletConstants.VARIANTS_TYPE_CONSTRAINT || !typeConstraint && ServletConstants.VARIANTS_TYPE_ANTIPATTERN)
-						new PatternTextImpl(existing, var);
-				}
-				if (ServletConstants.OLD_VARIANTS) {
-					for (String variant: oldvariants) {
-						new PatternTextImpl(existing, new JSONObject(variant));
-					}
-				}
-				ServletUtilities.saveTemplate(language.getLiteral(), id, existing);
+				ServletUtilities.loadTemplate(language.getLiteral(), id);
+//				CompletePattern existing = ServletUtilities.loadTemplate(language.getLiteral(), id);
+//				for (String variant: variants) {
+//					JSONObject var = new JSONObject(variant);
+//					Boolean typeConstraint = var.getBoolean(ConstantsJSON.TYPE_CONSTRAINT);
+//					if (typeConstraint && ServletConstants.VARIANTS_TYPE_CONSTRAINT || !typeConstraint && ServletConstants.VARIANTS_TYPE_ANTIPATTERN)
+//						new PatternTextImpl(existing, var);
+//				}
+//				if (ServletConstants.OLD_VARIANTS) {
+//					for (String variant: oldvariants) {
+//						new PatternTextImpl(existing, new JSONObject(variant));
+//					}
+//				}
+//				ServletUtilities.saveTemplate(language.getLiteral(), id, existing);
 				return;
 			} catch (IOException e) {}
 		}
@@ -114,38 +111,38 @@ public class PatternBundle {
 	
 		pattern.setAbstractId(pattern.getPatternId());
 		pattern.setPatternId(new_id);
-		List<Parameter> params = pattern.getParameterList().getParameters();
 	
 		if (ServletConstants.FILL_VALUES && values != null) {
+			List<Parameter> params = pattern.getParameterList().getParameters();
 			for (Integer index: values.keySet()) {
 				params.get(index).setValueFromString(values.get(index));
 			}
 		}
 	
-		if (variants != null) {
-			for (String json: variants) {
-				try {
-					JSONObject var = new JSONObject(json);
-					Boolean typeConstraint = var.getBoolean(ConstantsJSON.TYPE_CONSTRAINT);
-					if (typeConstraint && ServletConstants.VARIANTS_TYPE_CONSTRAINT || !typeConstraint && ServletConstants.VARIANTS_TYPE_ANTIPATTERN)
-						new PatternTextImpl(pattern, var);
-				} catch(JSONException e) {
-					ServletUtilities.logError(e);
-					ServletUtilities.log(" within JSON: " + json);
-				}
-			}
-		}
-	
-		if (ServletConstants.OLD_VARIANTS && oldvariants != null) {
-			for (String json: oldvariants) {
-				try {
-					new PatternTextImpl(pattern, new JSONObject(json));
-				} catch(JSONException e) {
-					ServletUtilities.logError(e);
-					ServletUtilities.log(" within JSON: " + json);
-				}
-			}
-		}
+//		if (variants != null) {
+//			for (String json: variants) {
+//				try {
+//					JSONObject var = new JSONObject(json);
+//					Boolean typeConstraint = var.getBoolean(ConstantsJSON.TYPE_CONSTRAINT);
+//					if (typeConstraint && ServletConstants.VARIANTS_TYPE_CONSTRAINT || !typeConstraint && ServletConstants.VARIANTS_TYPE_ANTIPATTERN)
+//						new PatternTextImpl(pattern, var);
+//				} catch(JSONException e) {
+//					ServletUtilities.logError(e);
+//					ServletUtilities.log(" within JSON: " + json);
+//				}
+//			}
+//		}
+//	
+//		if (ServletConstants.OLD_VARIANTS && oldvariants != null) {
+//			for (String json: oldvariants) {
+//				try {
+//					new PatternTextImpl(pattern, new JSONObject(json));
+//				} catch(JSONException e) {
+//					ServletUtilities.logError(e);
+//					ServletUtilities.log(" within JSON: " + json);
+//				}
+//			}
+//		}
 	
 		pattern.isValid(AbstractionLevel.ABSTRACT);
 		return pattern;
