65f4cc3d1d19b13a36ed559e2c3ce839374eca9c
Arno <arno.kesper@uni-marburg.de>
2026-03-31 16:46:43 +0200

    Add NewPatternInitialization.

---
+++ qualitypatternmodel/src/qualitypatternmodel/utility/NewPatternInitialization.java
@@ -0,0 +1,199 @@
+package qualitypatternmodel.utility;
+
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class NewPatternInitialization {
+	
+	static String info = "Description\r\n"
+			+ "Proposed identifier: ValidLinkIfMatch\r\n"
+			+ "Name: Valid Link by Pattern\r\n"
+			+ "Description: Check if field values match a specific pattern and are valid links.\r\n"
+			+ "Scope: simple\r\n"
+			+ "Type: ref\r\n"
+			+ "Template Text\r\n"
+			+ "If a <Element> has a <Property> that <Does / Does Not> match <a Specific Pattern (RegEx)>, this Property is a valid link.\r\n"
+			+ "Specific Example\r\n"
+			+ "If a <Record Source> has a <Legal Body ID> that <Does> match <^(((https?://)?\\\\\\[a-zA-Z0-9\\\\\\\\\\\\\\\\-\\\\\\\\\\\\\\\\./\\\\\\]\\\\\\*\\\\\\\\\\\\\\\\.\\\\\\[a-z\\\\\\]{2,3}/(\\\\\\[a-zA-Z0-9\\\\\\\\\\\\\\\\-\\\\\\\\\\\\\\\\./\\\\\\]\\\\\\*/)?)>, this Property is a valid link.\r\n"
+			+ "Example Values\r\n"
+			+ "If a <Digital Object> has a <Providing Institution> that <Does> match <^(((https?://)?\\\\\\[a-zA-Z0-9\\\\\\\\\\\\\\\\-\\\\\\\\\\\\\\\\./\\\\\\]\\\\\\*\\\\\\\\\\\\\\\\.\\\\\\[a-z\\\\\\]{2,3}/(\\\\\\[a-zA-Z0-9\\\\\\\\\\\\\\\\-\\\\\\\\\\\\\\\\./\\\\\\]\\\\\\*/)?)>, this Property is a valid link.";
+
+	static String variantid = "default";
+	static String language = "xml";
+	
+	static String id;
+	static String name;
+	static String description;
+	static String scope;
+	static String type;
+	static String sentence;
+	static String sentence_example;
+
+	public static void normalizeValues() {
+		sentence = sentence.trim().replace(" +", " ");
+		sentence_example = sentence_example.trim().replace(" +", " ");
+	}
+
+	public static String[] splitSentence(String sentence) {
+		String[] split = sentence.split("(?=<)|(?<=>)");
+		for (int i = split.length-1; i>=0; i--) {
+			split[i] = split[i].trim();
+		}
+		return split;
+	}
+
+	public static boolean isParameter(String val) {
+		return val.trim().startsWith("<") && val.trim().endsWith(">"); 
+	}
+
+	public static JSONObject makeFragment(String base, String example) {
+		JSONObject object = new JSONObject();
+		if (isParameter(base) && isParameter(example)) {
+			base = base.replaceAll("^<|>$", "");
+			example = example.replaceAll("^<|>$", "");
+			object.put(ConstantsJSON.NAME, base);
+			object.put(ConstantsJSON.EXAMPLEVALUE, example);
+			object.put(ConstantsJSON.PARAMETER, new JSONArray());
+			object.put(ConstantsJSON.DESCRIPTION, "");
+		}
+		else if (base.equals(example)) {
+			object.put(ConstantsJSON.TEXT, base);
+		}
+		else {
+			System.err.println("Fragments not fitting: '" + base + "' and '" + example + "'");
+		}
+		return object;
+	}
+
+	public static JSONArray makeFragments() {
+		normalizeValues();
+		
+		String[] base = splitSentence(sentence);
+		String[] example = splitSentence(sentence_example);
+		if (base.length != example.length) {
+			System.err.println(base.length);
+			System.err.println(example.length);
+			return null;
+		}
+		JSONArray array = new JSONArray();
+		for (int i = 0; i<base.length; i++) {
+			array.put(makeFragment(base[i], example[i]));
+		}
+		return array;
+	}
+
+	public static JSONObject makeVariant() {
+		JSONObject object = new JSONObject();
+		JSONObject custom = new JSONObject();
+		object.put(ConstantsJSON.CUSTOM, custom);
+		
+		object.put(ConstantsJSON.TEMPLATE, id + "_" + language);
+		object.put(ConstantsJSON.NAME, variantid);
+		object.put(ConstantsJSON.LANGUAGE, language);
+		object.put(ConstantsJSON.TYPE_CONSTRAINT, true);
+
+		custom.put(ConstantsJSON.DESCRIPTION, variantid + " variant for " + id + "_" + language);
+		custom.put("scope", scope);
+		custom.put("type", new JSONArray(type.split(", ")));
+		
+		object.put(ConstantsJSON.FRAGMENTS, makeFragments());
+		return object;
+	}
+
+	public static void saveVariant() throws IOException {
+		String path = "src/qualitypatternmodel/newservlets/jsons/";
+		String filename = id.toUpperCase() + "_" + language.toUpperCase() + "_" + variantid.toUpperCase() + ".json";
+		JSONObject variant = makeVariant();
+		Util.exportJson(variant, path + filename);
+		System.out.println(variant.toString(4));	
+	}
+	
+	public static void templateInfo() throws IOException {
+
+		JSONObject info = new JSONObject();
+		info.put(ConstantsJSON.ID, id);
+		info.put(ConstantsJSON.NAME, name);
+		info.put(ConstantsJSON.DESCRIPTION, description);
+		
+		
+		String path = "src/qualitypatternmodel/newservlets/template_info.json";
+		JSONObject template_info = Util.loadJson(path);
+		template_info.put(id + "Pattern", info);
+		Util.exportJson(template_info, path);
+
+		System.out.println("Added in template_info.json");
+		System.out.println("\"" + id + "Pattern\":" + info.toString(4));
+	}
+	
+	public static void patternConstants() throws IOException {
+		String path = "src/qualitypatternmodel/newservlets/initialisation/PatternConstants.java";
+		String res =  "\r\n" + 
+				"    public static String " + id.toUpperCase() + "_ID = \"" + id +  "\";\r\n" +
+				"    public static String " + id.toUpperCase() + "_NAME = \"" + name +  "\";\r\n" +
+				"    public static String " + id.toUpperCase() + "_DESCR = \"" + description +  "\";" +
+				"\r\n}";
+		
+		String file = Util.loadFile(path);
+		file = file.replaceAll("}\\s*$", "");
+		file = file + res;
+		Util.overrideFile(file, path);
+		
+		System.out.println("For PatternConstants.java");
+		System.out.println(res);
+		System.out.println();
+	}
+
+	public static void splitInfo() throws Exception {
+		String[] infos = info.split("\r\n\\s*");
+		for (int i = 0; i<infos.length; i++) {
+			String part = infos[i];
+			if (part.startsWith("Proposed identifier: ")) {
+				id = part.replace("Proposed identifier: ", "").trim();
+			}
+			else if (part.startsWith("Name: ")) {
+				name = part.replace("Name: ", "").trim();
+			}	
+			else if (part.startsWith("Description: ")) {
+				description = part.replace("Description: ", "").trim();
+			}
+			else if (part.startsWith("Scope: ")) {
+				scope = part.replace("Scope: ", "").trim();
+			}				
+			else if (part.startsWith("Type: ")) {
+				type = part.replace("Type: ", "").trim();
+			}
+			else if (part.equals("Template Text") && infos.length > i+1) {
+				sentence = infos[i+1].trim();
+			}				
+			else if (part.equals("Example Values") && infos.length > i+1) {
+				sentence_example = infos[i+1].trim();
+			}
+		}
+		if (id.isBlank()) 
+			throw new Exception("id blank");
+		if (name.isBlank())
+			throw new Exception("name blank");
+		if (description.isBlank())
+			throw new Exception("description blank");
+		if (scope.isBlank())
+			throw new Exception("scope blank");
+		if (type.isBlank())
+			throw new Exception("type blank");
+		if (sentence.isBlank())
+			throw new Exception("sentence blank");
+		if (sentence_example.isBlank())
+			throw new Exception("sentence_example blank");
+		
+		for (String s: infos)
+			System.out.println("- " + s);
+		System.out.println();
+	}
+
+	public static void main(String[] args) throws Exception {
+		splitInfo();
+		saveVariant();
+		patternConstants();
+		templateInfo();
+	}
+}
