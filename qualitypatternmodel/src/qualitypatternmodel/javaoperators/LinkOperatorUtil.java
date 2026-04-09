be2c38cb7d1139f2f3954aad412697fb807d51ab
Arno <arno.kesper@uni-marburg.de>
2026-04-09 13:40:59 +0200

    Remove unnecessary prints.

--- qualitypatternmodel/src/qualitypatternmodel/javaoperators/LinkOperatorUtil.java
+++ qualitypatternmodel/src/qualitypatternmodel/javaoperators/LinkOperatorUtil.java
@@ -144,7 +144,6 @@ public class LinkOperatorUtil {
 
         // Fallback to GET
         if (isFailure(code)) {
-        	System.out.println("FAILIURE");
             request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
                     .timeout(Duration.ofMillis(DEFAULT_TIMEOUT))
